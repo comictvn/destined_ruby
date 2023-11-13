523dbc5f68f43ac87cfa7754c04794d4675d5607
Bernadene (Jitera) <developers@jitera.com>
2023-11-13 06:17:01 +0000

    feat[business-logic]: implement 'Profile setup' Changes: * '/app/models/user.rb': : + Add new fields to the User model: name, age, gender, location, preferences, profile_picture, bio. + Add validations for these fields. + Write a method to update these fields and save the changes to the database. + Write a method to send a confirmation message to the user after successful profile...

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,30 @@
 class User < ApplicationRecord
+  # Remove attr_accessor as these are database fields
+  # attr_accessor :name, :age, :gender, :location, :preferences, :profile_picture, :bio
+  # Add validations
+  validates :name, :age, :gender, :location, :preferences, :profile_picture, :bio, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validates :gender, inclusion: { in: %w(male female other) }
+  validates :name, length: { maximum: 50 }
+  validates :bio, length: { maximum: 500 }
   has_secure_password
   enum gender: { male: 0, female: 1, other: 2 }
   has_many :social_media
+  # Method to update user profile
+  def update_profile(params)
+    self.name = params[:name]
+    self.age = params[:age]
+    self.gender = params[:gender]
+    self.location = params[:location]
+    self.preferences = params[:preferences]
+    self.profile_picture = params[:profile_picture]
+    self.bio = params[:bio]
+    save
+  end
+  # Method to send confirmation message
+  def send_confirmation_message
+    # Code to send confirmation message goes here
+  end
   def self.authenticate?(email, password)
     user = User.find_by(email: email)
     return user if user && user.authenticate(password)
