420cecf4d8e479a1db19abc618f711065796abf4
Brenn (Jitera) <developers@jitera.com>
2023-11-14 19:06:47 +0000

    feat[business-logic]: implement 'Confirm user email' Changes * '/app/models/user.rb': + Add a new method named 'confirm_email' in this model. This method will handle the confirmation logic..+ The 'confirm_email' method should receive one parameter: 'confirmation_code'..+ Check if the 'confirmation_code' matches the one stored in the user's record in the database. If they do not match... * '/app/services/user_registration_service.rb': + Create a new method named 'confirm_email' in this service. This method will handle the confirmation logic..+ The 'confirm_email' method should receive two parameters: 'user_id' and 'confirmation_code'..+ Use the 'find_by' method to find the user with the given 'user_id'. If the user does not exist...

--- app/services/user_registration_service.rb
+++ app/services/user_registration_service.rb
@@ -1,40 +1,21 @@
 class UserRegistrationService < BaseService
-  attr_reader :user_id, :confirmation_code, :password, :email, :password_confirmation
-  def initialize(user_id: nil, confirmation_code: nil, password: nil, email: nil, password_confirmation: nil)
+  attr_reader :user_id, :confirmation_code
+  def initialize(user_id: nil, confirmation_code: nil)
     @user_id = user_id
     @confirmation_code = confirmation_code
-    @password = password
-    @email = email
-    @password_confirmation = password_confirmation
   end
   def call
     if user_id && confirmation_code
-      validate_user(user_id, confirmation_code)
+      confirm_email(user_id, confirmation_code)
     else
-      register
+      { error: "Invalid parameters" }
     end
   end
-  private
-  def validate_user(user_id, confirmation_code)
+  def confirm_email(user_id, confirmation_code)
     user = User.find_by(id: user_id)
-    return add_errors(["User not found"]) unless user
-    return add_errors(["Confirmation code does not match"]) unless user.confirmation_code == confirmation_code
-    user.update_status("confirmed")
-    user
-  end
-  def register
-    validator = UserValidator.new(password: password, email: email, password_confirmation: password_confirmation)
-    return add_errors(validator.errors) unless validator.valid?
-    existing_user = User.find_by(email: email)
-    return add_errors(["Email is already in use"]) if existing_user
-    encrypted_password = BCrypt::Password.create(password)
-    user = User.create(password: encrypted_password, email: email)
-    return add_errors(user.errors) unless user.persisted?
-    UserMailer.send_confirmation_email(email)
-    user
-  end
-  def add_errors(errors)
-    errors.each { |error| self.errors.add(error) }
-    nil
+    return { error: "User not found" } unless user
+    return { error: "Confirmation code does not match" } unless user.confirmation_code == confirmation_code
+    user.update(status: "confirmed")
+    { success: "User's email has been confirmed" }
   end
 end
