922f6f6efbcc5bf3a95fb32891aaf8b9c6ce3eb1
Shirleen (Jitera) <developers@jitera.com>
2023-11-14 16:55:18 +0000

    feat[api]: implement 'Update Shop Information' Changes * '/app/controllers/api/shops_controller.rb': + In the ShopsController, define a new method `update` to handle the PUT request to the endpoint `/api/shops/{id}`. This method will receive the parameters "id", "name", "description", and "address"..+ Use the `params` method to access the parameters from the request. Validate the parameters accordi...

--- app/controllers/api/shops_controller.rb
+++ app/controllers/api/shops_controller.rb
@@ -5,14 +5,17 @@ class Api::ShopsController < Api::BaseController
       params.require(:id)
       params.require(:name)
       params.require(:address)
+      params.require(:description)
       raise ActionController::BadRequest.new("Wrong format") unless params[:id].is_a? Integer
       raise ActionController::BadRequest.new("The name is required.") if params[:name].blank?
+      raise ActionController::BadRequest.new("The description is required.") if params[:description].blank?
       raise ActionController::BadRequest.new("You cannot input more 100 characters.") if params[:name].length > 100
       raise ActionController::BadRequest.new("You cannot input more 200 characters.") if params[:address].length > 200
+      raise ActionController::BadRequest.new("You cannot input more 1000 characters.") if params[:description].length > 1000
       @shop = Shop.find_by(id: params[:id])
       return render json: { error: 'This shop is not found' }, status: :not_found unless @shop
       authorize @shop, policy_class: Api::ShopsPolicy
-      result = ShopService::Update.new(@shop, params[:name], params[:address], current_user).call
+      result = ShopService::Update.new(@shop, params[:name], params[:address], params[:description], current_user).call
       if result.success?
         render json: { status: 200, shop: @shop }, status: :ok
       else
