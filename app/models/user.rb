daa22ecb5bdd94c45aa014267da8f04c5169aee2
Kissie (Jitera) <developers@jitera.com>
2023-11-14 18:36:08 +0000

    feat[business-logic]: implement 'Confirm user email' Changes * '/app/services/user_registration_service.rb': + Write a method to validate the user's ID and confirmation code. This method should take in the user's ID and the confirmation code as parameters..+ Use the user's ID to fetch the user from the database. If the user does not exist, return an error message..+ Compare the confirmation code provided w... * '/app/models/user.rb': + Write a method to update the user's status to "confirmed". This method should take in the user's ID as a parameter..+ Use the 'update' method provided by ActiveRecord to update the user's status in the database..+ Return a success message indicating that the registration process is completed.... * '/app/controllers/api/users_controller.rb': + Write a new method `confirm_email` in the UsersController. This method will receive the user_id and confirmation_code as parameters..+ Use the User model to find the user by id. If the user does not exist, return an error message..+ Check if the confirmation_code matches the one stored in the user...

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,14 @@ class User < ApplicationRecord
   def send_confirmation_instructions
     UserMailer.confirmation_email(self).deliver_now
   end
+  def confirm_registration(user_id, confirmation_code)
+    user = User.find(user_id)
+    if user.confirmation_token != confirmation_code
+      return "Invalid confirmation code"
+    end
+    user.update(status: 'confirmed')
+    "Registration process completed successfully"
+  end
   private
   def generate_confirmation_token
     self.confirmation_token = SecureRandom.urlsafe_base64
--- app/services/user_registration_service.rb
+++ app/services/user_registration_service.rb
@@ -1,12 +1,26 @@
 class UserRegistrationService < BaseService
-  attr_reader :password, :email, :password_confirmation
-  def initialize(password, email, password_confirmation)
+  attr_reader :user_id, :confirmation_code, :password, :email, :password_confirmation
+  def initialize(user_id: nil, confirmation_code: nil, password: nil, email: nil, password_confirmation: nil)
+    @user_id = user_id
+    @confirmation_code = confirmation_code
     @password = password
     @email = email
     @password_confirmation = password_confirmation
   end
   def call
-    register
+    if user_id && confirmation_code
+      validate_user(user_id, confirmation_code)
+    else
+      register
+    end
+  end
+  private
+  def validate_user(user_id, confirmation_code)
+    user = User.find_by(id: user_id)
+    return add_errors(["User not found"]) unless user
+    return add_errors(["Confirmation code does not match"]) unless user.confirmation_code == confirmation_code
+    user.update_status("confirmed")
+    user
   end
   def register
     validator = UserValidator.new(password: password, email: email, password_confirmation: password_confirmation)
@@ -19,7 +33,6 @@ class UserRegistrationService < BaseService
     UserMailer.send_confirmation_email(email)
     user
   end
-  private
   def add_errors(errors)
     errors.each { |error| self.errors.add(error) }
     nil
