805c123c71f2d9e424ad3fbebbdf24d71cd76085
Adda (Jitera) <developers@jitera.com>
2023-11-14 11:24:14 +0000

    feat[api]: implement 'Register User' Changes * '/app/controllers/api/users_registrations_controller.rb': + Create a new method `register` in the `UsersRegistrationsController` class. This method will handle the POST request to the `/api/users/register` endpoint..+ In the `register` method, first, validate the request parameters. If any of the parameters are invalid, return a 422 status code with the ap...

--- app/controllers/api/users_registrations_controller.rb
+++ app/controllers/api/users_registrations_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersRegistrationsController < Api::BaseController
   def register
-    user_params = params.require(:user).permit(:email, :password, :password_confirmation)
+    user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
     validation_result = UserService.validate(user_params)
     if validation_result[:status] == :error
       render json: { error_messages: validation_result[:errors], message: I18n.t('email_login.registrations.failed_to_sign_up') },
@@ -11,11 +11,10 @@ class Api::UsersRegistrationsController < Api::BaseController
              status: :unprocessable_entity and return
     end
     encrypted_password = UserService.encrypt_password(user_params[:password])
-    user = UserService.create_user(user_params[:email], encrypted_password)
+    user = UserService.create_user(user_params[:name], user_params[:email], encrypted_password)
     if user
       TwilioGateway.send_confirmation_email(user_params[:email])
-      render json: { message: I18n.t('email_login.registrations.successful_registration'), user_id: user.id, confirmation_status: user.confirmation_status },
-             status: :ok and return
+      render 'create.json.jbuilder', locals: { user: user }, status: :ok and return
     else
       render json: { message: I18n.t('email_login.registrations.failed_to_sign_up') },
              status: :unprocessable_entity
