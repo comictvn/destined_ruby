e40f148784b03c05035f5498eb766668582b75ac
Florie (Jitera) <developers@jitera.com>
2023-11-14 18:35:31 +0000

    feat[business-logic]: implement 'Update shop information' Changes * '/app/policies/api/shops_policy.rb': + In the ShopsPolicy class, create a new method called 'update?' which will check if the current user has the permission to update the shop's information..+ If the user does not have the permission, return an error message..+ If the user has the permission, return true.... * '/app/controllers/api/shops_controller.rb': + In the ShopsController, create a new method called 'update' which will handle the update request. This method should take in the parameters "shop_id", "name", and "address"..+ Use the 'find' method to locate the shop with the given ID in the database. If the shop does not exist, return an error me... * '/app/validators/user_validator.rb': + In the UserValidator class, create a new method called 'validate_shop_update' which will handle the validation for the update request..+ This method should receive the shop's ID, name, and address as parameters..+ Use the 'validates_presence_of' method to check if the shop's ID, name, and address ... * '/app/services/shop_service/update.rb': + In the ShopService::Update class, create a new method called 'call' which will handle the update process. This method should take in the parameters "shop_id", "name", and "address"..+ Use the 'find' method to locate the shop with the given ID in the database. If the shop does not exist, return an ...

--- app/services/shop_service/update.rb
+++ app/services/shop_service/update.rb
@@ -1,23 +1,23 @@
 # PATH: /app/services/shop_service/update.rb
 # rubocop:disable Style/ClassAndModuleChildren
 class ShopService::Update
-  attr_accessor :id, :name, :address, :current_user
-  def initialize(id, name, address, current_user)
-    @id = id
+  attr_accessor :shop_id, :name, :address, :current_user
+  def initialize(shop_id, name, address, current_user)
+    @shop_id = shop_id
     @name = name
     @address = address
     @current_user = current_user
   end
   def call
     validate_input
-    shop = Shop.find_by(id: @id)
+    shop = Shop.find_by(id: @shop_id)
     raise StandardError, 'This shop is not found' unless shop
     unless ShopsPolicy.new(@current_user, shop).update?
       raise StandardError, 'You do not have permission to update this shop'
     end
     begin
       shop.update!(name: @name, address: @address)
-      shop
+      { message: 'Shop updated successfully', shop: shop }
     rescue StandardError => e
       raise StandardError, 'An unexpected error occurred', message: e.message
     end
@@ -25,8 +25,8 @@ class ShopService::Update
   private
   def validate_input
     errors = []
-    errors << 'ID is required' if @id.blank?
-    errors << 'ID is not a number' unless @id.is_a?(Numeric)
+    errors << 'Shop ID is required' if @shop_id.blank?
+    errors << 'Shop ID is not a number' unless @shop_id.is_a?(Numeric)
     errors << 'The name is required.' if @name.blank?
     errors << 'You cannot input more 100 characters.' if @name.length > 100
     errors << 'Address is required' if @address.blank?
