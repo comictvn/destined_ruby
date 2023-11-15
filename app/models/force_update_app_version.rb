4f14be85224735d1f6d021b5be02810d08b86991
Kimmie (Jitera) <developers@jitera.com>
2023-11-15 17:14:44 +0000

    feat[database]: delete tables 'force_update_app_versions', 'matchs', 'reactions', 'active_storage_blobs', 'active_storage_attachments', 'active_storage_variant_records', 'messages', 'oauth_applications', 'oauth_access_grants', 'oauth_access_tokens', 'chanels', 'user_chanels', 'users' Changes + '/app/controllers/api/chanels/messages_controller.rb': deleted + '/app/controllers/api/chanels_controller.rb': deleted + '/app/controllers/api/force_update_app_versions_controller.rb': deleted + '/app/models/chanel.rb': deleted + '/app/models/force_update_app_version.rb': deleted + '/app/models/match.rb': deleted + '/app/models/message.rb': deleted + '/app/models/reaction.rb': deleted + '/app/models/user_chanel.rb': deleted + '/app/policies/api/chanels/messages_policy.rb': deleted + '/app/services/chanel_service/index.rb': deleted + '/app/services/force_update_app_version_service/index.rb': deleted + '/app/services/message_service/index.rb': deleted + '/app/views/api/chanels/destroy.json.jbuilder': deleted + '/app/views/api/chanels/index.json.jbuilder': deleted + '/app/views/api/chanels/messages/destroy.json.jbuilder': deleted + '/app/views/api/chanels/messages/index.json.jbuilder': deleted + '/app/views/api/chanels/show.json.jbuilder': deleted + '/app/views/api/force_update_app_versions/index.json.jbuilder': deleted + '/app/views/api/messages/create.json.jbuilder': deleted + '/spec/factories/chanels.rb': deleted + '/spec/factories/force_update_app_versions.rb': deleted + '/spec/factories/matchs.rb': deleted + '/spec/factories/messages.rb': deleted + '/spec/factories/reactions.rb': deleted + '/spec/factories/user_chanels.rb': deleted + '/spec/factories/users.rb': deleted + '/spec/requests/api/chanels/messages_spec.rb': deleted + '/app/models/user.rb': deleted

--- app/models/force_update_app_version.rb
+++
@@ -1,7 +0,0 @@
-class ForceUpdateAppVersion < ApplicationRecord
-  enum platform: %w[ios android], _suffix: true
-
-  validates :reason, length: { in: 0..65_535 }, if: :reason?
-  validates :version, presence: true
-  validates :version, length: { in: 0..255 }, if: :version?
-end
--- spec/factories/reactions.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :reaction do
-    user { create(:user) }
-
-    user { create(:user) }
-
-    react_type { Reaction.react_types.keys[0] }
-  end
-end
