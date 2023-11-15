5982e03b077b8ae30d91fb6ab1e2f3be6a849db8
Natalya (Jitera) <developers@jitera.com>
2023-11-15 18:35:58 +0000

    feat[api]: implement 'Update Shop Information' Changes * '/app/controllers/api/v1/shops_controller.rb': + In the ShopsController, create an 'update' method that will handle the PUT request to update the shop information..+ The 'update' method should accept the shop 'id', 'name', and 'address' as parameters from the request body..+ Use the 'ShopService' to update the shop information. Pass the 'id', 'n...

---
+++ app/controllers/api/v1/shops_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::ShopsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_shop, only: [:update]
+  before_action :authorize_user, only: [:update]
+  def update
+    begin
+      shop_params = params.permit(:id, :name, :address)
+      result = ShopValidator.new(shop_params).validate
+      if result[:status] == :error
+        render json: { error: result[:message] }, status: :unprocessable_entity
+      else
+        shop = ShopService.new(shop_params).update
+        if shop[:status] == :error
+          render json: { error: shop[:message] }, status: :unprocessable_entity
+        else
+          render json: { status: 200, message: 'Shop updated successfully.', shop: shop[:data] }, status: :ok
+        end
+      end
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+  private
+  def set_shop
+    @shop = Shop.find_by(id: params[:id])
+    return render json: { error: 'This shop is not found' }, status: :not_found unless @shop
+  end
+  def authorize_user
+    unless ShopPolicy.new(current_user, @shop).update?
+      render json: { error: 'You are not authorized to update this shop.' }, status: :forbidden
+    end
+  end
+end
