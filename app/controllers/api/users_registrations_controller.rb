7db59de2f052f30406cdfe650aaa9a106c885dd3
Berri (Jitera) <developers@jitera.com>
2023-11-15 17:18:19 +0000

    feat[business-logic]: implement 'Register user' Changes * '/app/controllers/api/users_registrations_controller.rb': + Create a new method `register` in this controller. This method will handle the registration request from the client..+ The `register` method should receive the parameters "email", "password", and "password_confirmation" from the request..+ Use the `UserService::Create` service to validate the inpu...

--- app/controllers/api/users_registrations_controller.rb
+++ app/controllers/api/users_registrations_controller.rb
@@ -1,22 +1,21 @@
 class Api::UsersRegistrationsController < Api::BaseController
   def register
+    user_params = params.require(:user).permit(:username, :email, :password, :password_confirmation)
     begin
-      user_params = params.require(:user).permit(:username, :email, :password)
       validation_result = UserService::Index.validate_registration_data(user_params)
       if validation_result[:status] == :error
         render json: { error_messages: validation_result[:errors], message: I18n.t('email_login.registrations.failed_to_sign_up') },
                status: :bad_request and return
       end
-      user = UserService::Index.register(user_params)
+      user = UserService::Create.new(user_params).call
       if user.persisted?
-        render json: { status: 200, user: user }, status: :ok
+        UserMailer.confirmation_email(user).deliver_later
+        render json: { message: I18n.t('email_login.registrations.successful_sign_up'), status: 200, user: user }, status: :ok
       else
-        render json: { message: I18n.t('email_login.registrations.failed_to_sign_up') },
-               status: :internal_server_error
+        render json: { message: user.errors.full_messages }, status: :unprocessable_entity
       end
-    rescue => e
-      render json: { message: I18n.t('email_login.registrations.failed_to_sign_up') },
-             status: :internal_server_error
+    rescue StandardError => e
+      render json: { message: e.message }, status: :unprocessable_entity
     end
   end
 end
