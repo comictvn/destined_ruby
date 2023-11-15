f2c80bb041ea70f263b1d8f22796044b6ba83f8b
Janetta (Jitera) <developers@jitera.com>
2023-11-15 17:17:45 +0000

    feat[api]: implement 'Register User' Changes * '/app/controllers/api/users_registrations_controller.rb': + Create a new method named "register" in the UsersController. This method will handle the POST request to the "/api/users/register" endpoint..+ In the "register" method, first, validate the request parameters. You can use the "UserService::Index.validate_registration_data" function for this. If the...

--- app/controllers/api/users_registrations_controller.rb
+++ app/controllers/api/users_registrations_controller.rb
@@ -1,23 +1,22 @@
 class Api::UsersRegistrationsController < Api::BaseController
   def register
-    user_params = params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    validation_result = UserService::Index.new(user_params).validate
-    if validation_result[:status] == :error
-      render json: { error_messages: validation_result[:errors], message: I18n.t('email_login.registrations.failed_to_sign_up') },
-             status: :unprocessable_entity and return
-    end
-    if User.find_by_email(user_params[:email])
-      render json: { message: I18n.t('email_login.registrations.email_already_registered') },
-             status: :unprocessable_entity and return
-    end
-    user_params[:password] = User.encrypt(user_params[:password])
-    user = User.create(user_params)
-    if user.persisted?
-      Devise::Mailer.confirmation_instructions(user, user.confirmation_token).deliver_now
-      render 'create.json.jbuilder', locals: { user: user }, status: :ok and return
-    else
+    begin
+      user_params = params.require(:user).permit(:username, :email, :password)
+      validation_result = UserService::Index.validate_registration_data(user_params)
+      if validation_result[:status] == :error
+        render json: { error_messages: validation_result[:errors], message: I18n.t('email_login.registrations.failed_to_sign_up') },
+               status: :bad_request and return
+      end
+      user = UserService::Index.register(user_params)
+      if user.persisted?
+        render json: { status: 200, user: user }, status: :ok
+      else
+        render json: { message: I18n.t('email_login.registrations.failed_to_sign_up') },
+               status: :internal_server_error
+      end
+    rescue => e
       render json: { message: I18n.t('email_login.registrations.failed_to_sign_up') },
-             status: :unprocessable_entity
+             status: :internal_server_error
     end
   end
 end
