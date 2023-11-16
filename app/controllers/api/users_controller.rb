25650a458c31fd64a90200e669a2d3586e178b41
Genny (Jitera) <developers@jitera.com>
2023-11-16 15:44:43 +0000

    feat[business-logic]: implement 'Retrieve potential matches' Changes * '/app/controllers/api/users_controller.rb': + Write a new method `get_potential_matches` in the UsersController. This method will handle the incoming request for potential matches..+ The method should accept two parameters: "id" and "preferences". The "id" will be used to identify the user making the request, and the "preferences" will be use... * '/app/services/user_service/get_potential_matches.rb': + Create a new service `GetPotentialMatches` in the UserService. This service will handle the business logic of finding potential matches..+ The service should accept "id" and "preferences" as parameters..+ Use the "id" to retrieve the user's preferences from the "users" table in the database. You c...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,21 +1,37 @@
 class Api::UsersController < ApplicationController
-  def find_potential_matches
-    begin
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
+  rescue_from ActionController::BadRequest, with: :bad_request
+  def get_potential_matches
+    ActiveRecord::Base.transaction do
       params.require(:id)
       raise ActionController::BadRequest.new("Wrong format") unless params[:id].is_a? Integer
-      user = User.find_by(id: params[:id])
-      return render json: { error: 'User not found' }, status: :not_found unless user
-      preferences = user.preferences
-      potential_matches = User.where(preferences: preferences).where.not(id: user.id)
+      user = User.find(params[:id])
+      preferences = params[:preferences] || user.preferences
+      potential_matches = User.where(preferences: preferences)
+                              .where.not(id: user.id)
+                              .where.not(id: Match.where(user_id: user.id).pluck(:match_id))
       if potential_matches.empty?
         render json: { message: 'No more potential matches' }, status: :ok
       else
-        render json: { matches: potential_matches }, status: :ok
+        total_matches = potential_matches.count
+        total_pages = (total_matches / 10.0).ceil
+        render json: { matches: potential_matches.pluck(:id, :name, :preferences), total_matches: total_matches, total_pages: total_pages }, status: :ok
       end
-    rescue ActionController::ParameterMissing => e
-      render json: { error: e.message }, status: :bad_request
-    rescue => e
-      render json: { error: 'Something went wrong' }, status: :internal_server_error
     end
+  end
+  private
+  def record_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
+  def record_invalid(exception)
+    render json: { error: exception.message }, status: :unprocessable_entity
+  end
+  def parameter_missing(exception)
+    render json: { error: exception.message }, status: :bad_request
+  end
+  def bad_request(exception)
+    render json: { error: exception.message }, status: :bad_request
   end
 end
