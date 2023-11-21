6c53082373599296614bcc5e138434c79b9f1996
Aloysia (Jitera) <developers@jitera.com>
2023-11-21 04:34:43 +0000

    feat[business-logic]: implement 'Handle connection loss' Changes * '/app/controllers/application_controller.rb': + Create a before_action method to check the user's internet connection status. This method can use a third-party service or a custom method to detect the connection status..+ If the connection is lost, stop the current action and send a notification to the user. This can be done by rendering a JSON...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
+require 'net/http'
 class ApplicationController < ActionController::Base
+  before_action :check_internet_connection
+  private
+  def check_internet_connection
+    uri = URI('http://www.google.com/')
+    begin
+      response = Net::HTTP.get_response(uri)
+      if response.code != "200"
+        render json: { status: 'offline', message: 'Internet connection lost. Please check your connection.' }, status: 503
+      end
+    rescue
+      render json: { status: 'offline', message: 'Internet connection lost. Please check your connection.' }, status: 503
+    end
+  end
 end
