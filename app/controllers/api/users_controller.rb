bdd7d7a14973e3a7eac27faef2f2834d0c6b393f
Elysee (Jitera) <developers@jitera.com>
2023-11-21 07:41:11 +0000

    feat[business-logic]: implement 'Handle connection loss' Changes * '/app/controllers/api/users_controller.rb': + Create a new method `check_connection` in the UsersController. This method will be responsible for checking the user's internet connection status..+ Use a third-party service or library to detect the connection status. If the connection is lost, stop the process of retrieving potential matches for...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,44 +1,22 @@
+require 'net/http'
 class Api::UsersController < ApplicationController
   # Other methods...
-  def register
+  def check_connection
     begin
-      user_params = params.require(:user).permit(:username, :email, :password, :password_confirmation)
-      validate_user_params(user_params)
-      user = UserRegistrationService.new(user_params).register
-      UserMailer.confirmation_email(user).deliver_now
-      render json: UserSerializer.new(user).serialized_json, status: :ok
-    rescue UserValidator::ValidationError => e
-      render json: { error: e.message }, status: :bad_request
-    rescue ActiveRecord::RecordNotUnique => e
-      render json: { error: 'Email is already registered.' }, status: :conflict
-    rescue => e
-      render json: { error: 'Unexpected error occurred' }, status: :internal_server_error
-    end
-  end
-  def confirm_email
-    begin
-      email = params[:email]
-      confirmation_code = params[:confirmation_code]
-      UserValidator.new(email, confirmation_code).validate
-      if UserRegistrationService.new.confirm_email(email, confirmation_code)
-        render json: { status: 200, message: 'Email confirmed successfully' }, status: :ok
+      uri = URI('http://example.com')
+      res = Net::HTTP.get_response(uri)
+      if res.is_a?(Net::HTTPSuccess)
+        current_user.update(connection_status: true)
+        message = 'Internet connection is active.'
       else
-        render json: { error: 'An unexpected error occurred on the server' }, status: :internal_server_error
+        current_user.update(connection_status: false)
+        TwilioGateway.new.send_sms(current_user.phone_number, 'Your internet connection is lost.')
+        message = 'Internet connection is lost.'
       end
-    rescue UserValidator::ValidationError => e
-      render json: { error: e.message }, status: :bad_request
+      render json: { connection_status: current_user.connection_status, message: message }, status: :ok
     rescue => e
       render json: { error: 'Unexpected error occurred' }, status: :internal_server_error
     end
   end
-  private
-  def validate_user_params(user_params)
-    raise UserValidator::ValidationError, 'The username is required.' if user_params[:username].blank?
-    raise UserValidator::ValidationError, 'You cannot input more 50 characters.' if user_params[:username].length > 50
-    raise UserValidator::ValidationError, 'The password is required.' if user_params[:password].blank?
-    raise UserValidator::ValidationError, 'Password must be at least 8 characters.' if user_params[:password].length < 8
-    raise UserValidator::ValidationError, 'The email is required.' if user_params[:email].blank?
-    raise UserValidator::ValidationError, 'Invalid email format.' unless user_params[:email] =~ URI::MailTo::EMAIL_REGEXP
-    raise UserValidator::ValidationError, 'Password confirmation does not match.' if user_params[:password] != user_params[:password_confirmation]
-  end
+  # Other methods...
 end
