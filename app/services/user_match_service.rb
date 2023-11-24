b41fb3f04a292668872a68e5ba442f3343475867
Lindi (Jitera) <developers@jitera.com>
2023-11-24 17:12:04 +0000

    feat[business-logic]: implement 'Retrieve potential matches' Changes * '/app/services/user_match_service.rb': + Create a new method named `retrieve_potential_matches` with input parameters "id" and "preferences". This method will be responsible for retrieving potential matches based on the user's preferences..+ Use the provided user ID to fetch the user's preferences from the "users" table in the database. ...

--- app/services/user_match_service.rb
+++ app/services/user_match_service.rb
@@ -5,11 +5,14 @@ class UserMatchService < BaseService
   end
   def find_potential_matches
     user = User.find(@id)
-    potential_matches = User.where(@preferences)
+    potential_matches = User.where(@preferences).where.not(id: @id)
     if potential_matches.empty?
       { message: 'No more potential matches' }
     else
-      potential_matches
+      total_matches = potential_matches.count
+      potential_matches = potential_matches.paginate(page: 1, per_page: 10)
+      total_pages = potential_matches.total_pages
+      { matches: potential_matches, total_matches: total_matches, total_pages: total_pages }
     end
   end
 end
