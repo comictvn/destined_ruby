0a7eea156059cc17761d9581509aa53d87877b6f
Ciel (Jitera) <developers@jitera.com>
2023-11-29 16:45:36 +0000

    feat[business-logic]: implement 'Update user profile' Changes * '/app/controllers/api/users_controller.rb': + In the UsersController, create a new method called 'update_profile'. This method will be responsible for handling the update user profile request..+ The 'update_profile' method should accept the following parameters: "id", "age", "gender", "location", "interests", "preferences"..+ Use the 'params'...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,28 @@
 class Api::UsersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index show]
-
+  before_action :doorkeeper_authorize!, only: %i[index show update_profile]
+  before_action :set_user, only: %i[show update_profile]
+  before_action :authorize_user, only: %i[show update_profile]
   def index
-    # inside service params are checked and whiteisted
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
   end
-
   def show
+  end
+  def update_profile
+    if @user.update(user_params)
+      render json: {success: true, message: 'Profile updated successfully'}
+    else
+      render json: {success: false, message: @user.errors.full_messages.join(', ')}
+    end
+  end
+  private
+  def set_user
     @user = User.find_by!('users.id = ?', params[:id])
-
+  end
+  def authorize_user
     authorize @user, policy_class: Api::UsersPolicy
+  end
+  def user_params
+    params.permit(:age, :gender, :location, :interests, :preferences)
   end
 end
