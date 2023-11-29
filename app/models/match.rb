20aa1261bab667a5081d1ecb084fb20b4ab7068e
Mirabel (Jitera) <developers@jitera.com>
2023-11-29 16:44:07 +0000

    feat[database]: update table 'messages' and update table 'users' and create table 'matches' Changes * '/app/models/match.rb': created * '/app/models/message.rb': created * '/db/migrate/20230924155648_add_preferences_to_users.rb': created * '/db/migrate/20230924155648_create_matches_table.rb': created * '/db/migrate/20230924155648_update_table_messages.rb': created * '/app/models/user.rb': updated

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,8 +1,9 @@
 class Match < ApplicationRecord
   self.table_name = 'matchs'
-
   belongs_to :matcher1,
              class_name: 'User'
   belongs_to :matcher2,
              class_name: 'User'
+  has_many :messages, dependent: :destroy
+  validates :compatibility_score, presence: true
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,11 +1,17 @@
 class Message < ApplicationRecord
-  belongs_to :sender,
-             class_name: 'User'
   belongs_to :chanel
-
+  belongs_to :sender, class_name: 'User'
+  belongs_to :user
+  belongs_to :match
   has_many_attached :images, dependent: :destroy
-
-  validates :content, length: { in: 0..65_535 }, if: :content?
+  # validations
+  validates :content, presence: true, length: { in: 0..65_535 }, if: :content?
+  validates :sender_id, presence: true
+  validates :chanel_id, presence: true
+  validates :user_id, presence: true
+  validates :match_id, presence: true
+  validates :timestamp, presence: true
   validates :images, content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image/svg+xml'],
                      size: { less_than_or_equal_to: 100.megabytes }
+  # end for validations
 end
