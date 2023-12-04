9b1aea6d491f3c10d338c3b67ff3f68481474b33
Barrie (Jitera) <developers@jitera.com>
2023-12-04 17:13:10 +0000

    feat[business-logic]: implement 'Create message' Changes * '/app/models/message.rb': + Create a new method `create_message` in this model. This method will create a new message in the "messages" table with the provided content, user_id, and chanel_id..+ Return the created message's id.... * '/app/controllers/api/messages_controller.rb': + Create a new method `create` in this controller. This method will receive the parameters "content", "user_id", and "chanel_id" from the request..+ Use the `MessageService::Index` service to validate the input data. Ensure that the content is not empty, and the user_id and chanel_id exist in the da... * '/app/services/message_service/index.rb': + Create a new method `validate_input` in this service. This method will validate the input data, ensuring that the content is not empty, and the user_id and chanel_id exist in the database..+ If the validation is not successful, raise an error with a suitable message....

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,20 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chanel
-  belongs_to :sender, class_name: 'User'
+  belongs_to :sender, class_name: 'User', foreign_key: 'user_id'
   has_many_attached :images, dependent: :destroy
-  validates :content, presence: true, length: { in: 0..65_535 }, if: :content?
+  validates :content, presence: true, length: { in: 0..65_535 }
   validates :sender_id, presence: true
   validates :chanel_id, presence: true
   validates :user_id, presence: true
   validates :images, content_type: ['image/png', 'image/jpg', 'image/jpeg', 'image/gif', 'image/svg+xml'],
                      size: { less_than_or_equal_to: 100.megabytes }
+  def self.create_message(content, user_id, chanel_id)
+    message = self.new(content: content, user_id: user_id, chanel_id: chanel_id)
+    if message.save
+      return message.id
+    else
+      return nil
+    end
+  end
 end
