dd300f4272ba31463d40e9565060c809921b39c7
Avie (Jitera) <developers@jitera.com>
2023-12-04 10:56:04 +0000

    feat[business-logic]: implement 'Register user with phone number' Changes * '/config/routes.rb': + Add a new route to handle the registration request with a phone number. The route should point to the `create_with_phone` method of the `UsersRegistrationsController` class..+ The route should be a POST route and should be nested under the `users` resource.... * '/app/controllers/api/users_registrations_controller.rb': + Create a new method `register_with_phone` in the `UsersRegistrationsController` class. This method will handle the registration process with phone number..+ The method should accept three parameters: "name", "phone_number", and "password"..+ Use the `UserService::Index` service to check if the pho... * '/app/services/user_service/index.rb': + Create a new method `find_by_phone` in the `UserService::Index` class. This method will be used to check if a phone number is already registered in the "users" table..+ The method should receive the phone number as a parameter and use the `find_by` method of the `User` model to find a user with th...

--- app/controllers/api/users_registrations_controller.rb
+++ app/controllers/api/users_registrations_controller.rb
@@ -15,7 +15,26 @@ class Api::UsersRegistrationsController < Api::BaseController
              status: :unprocessable_entity
     end
   end
-
+  def register_with_phone
+    begin
+      user = UserService::Index.call(phone_number: params[:phone_number])
+      if user.present?
+        render json: { message: I18n.t('phone_login.registrations.phone_number_already_registered') }, status: :unprocessable_entity and return
+      else
+        encrypted_password = Devise::Encryptor.digest(User, params[:password])
+        @user = User.new(name: params[:name], phone_number: params[:phone_number], encrypted_password: encrypted_password)
+        if @user.save
+          render json: { message: I18n.t('common.200') }, status: :ok and return
+        else
+          error_messages = @user.errors.messages
+          render json: { error_messages: error_messages, message: I18n.t('phone_login.registrations.failed_to_sign_up') }, status: :unprocessable_entity
+        end
+      end
+    rescue => e
+      render json: { message: e.message }, status: :internal_server_error
+    end
+  end
+  private
   def create_params
     params.require(:user).permit(:password, :password_confirmation, :phone_number, :firstname, :lastname, :dob,
                                  :gender, :email)
