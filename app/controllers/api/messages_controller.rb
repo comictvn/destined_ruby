d42cc8491e69bc4c6566a854b8154a78f96b2e19
Jaine (Jitera) <developers@jitera.com>
2023-12-04 10:51:33 +0000

    feat[business-logic]: implement 'Create message' Changes * '/app/controllers/api/messages_controller.rb': + Create a new method named `create` in the MessagesController. This method will handle the POST request to /api/messages..+ Use strong parameters to permit only the "content" field from the request body. This can be done by defining a private method named `message_params` that returns `params.requi... * '/app/views/api/messages/create.json.jbuilder': + Create a new Jbuilder view named `create.json.jbuilder` under the `app/views/api/messages` directory..+ In this view, use the `json.extract!` method to extract the "id", "content", "created_at", and "updated_at" fields from the `@message` instance variable. This will be the JSON response returned ...

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,19 +1,16 @@
 class Api::MessagesController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[create]
-
   def create
-    @message = Message.new(create_params)
-
+    @message = Message.new(message_params)
     authorize @message, policy_class: Api::MessagesPolicy
-
-    return if @message.save
-
-    @error_object = @message.errors.messages
-
-    render status: :unprocessable_entity
+    if @message.save
+      render json: @message, status: :created
+    else
+      render json: { errors: @message.errors.messages }, status: :unprocessable_entity
+    end
   end
-
-  def create_params
-    params.require(:messages).permit(:content, :sender_id, :chanel_id, :images)
+  private
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
