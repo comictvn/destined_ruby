6663ecaca945f4260f34ebe256fd6c8d13ff6860
Nonnah (Jitera) <developers@jitera.com>
2023-12-04 12:41:48 +0000

    feat[business-logic]: implement 'Get force update app versions' Changes * '/app/controllers/api/force_update_app_versions_controller.rb': + In the ForceUpdateAppVersionsController, define a new method called 'index' which will handle the incoming GET request. This method will not require any input parameters..+ Call the ForceUpdateAppVersionService's 'index' method from within the 'index' method of the controller. This service method ... * '/app/views/api/force_update_app_versions/index.json.jbuilder': + In the 'index.json.jbuilder' view, iterate over each record in the passed local variable..+ For each record, create a JSON object with the keys 'version', 'created_at', and 'updated_at'. The values for these keys should be the corresponding attributes of the record..+ The resulting JSON array of o... * '/app/services/force_update_app_version_service/index.rb': + In the ForceUpdateAppVersionService, define a new method called 'index'. This method will handle the business logic of retrieving all records from the 'force_update_app_versions' table..+ Use the ActiveRecord 'all' method on the ForceUpdateAppVersion model to retrieve all records..+ Return the ret...

--- app/services/force_update_app_version_service/index.rb
+++ app/services/force_update_app_version_service/index.rb
@@ -1,72 +1,20 @@
 # rubocop:disable Layout/LineLength
 # rubocop:disable Style/ClassAndModuleChildren
 class ForceUpdateAppVersionService::Index
-  attr_accessor :params, :records, :query
-
-  def initialize(params, _current_user = nil)
-    @params = params
-
-    @records = ForceUpdateAppVersion
+  attr_accessor :records
+  def initialize
+    @records = ForceUpdateAppVersion.all
   end
-
   def execute
-    platform_equal
-
-    reason_start_with
-
-    version_start_with
-
-    force_update_equal
-
     order
-
     paginate
   end
-
-  def platform_equal
-    return if params.dig(:force_update_app_versions, :platform).blank?
-
-    @records = ForceUpdateAppVersion.where('platform = ?', params.dig(:force_update_app_versions, :platform))
-  end
-
-  def reason_start_with
-    return if params.dig(:force_update_app_versions, :reason).blank?
-
-    @records = if records.is_a?(Class)
-                 ForceUpdateAppVersion.where(value.query)
-               else
-                 records.or(ForceUpdateAppVersion.where('reason like ?', "%#{params.dig(:force_update_app_versions, :reason)}"))
-               end
-  end
-
-  def version_start_with
-    return if params.dig(:force_update_app_versions, :version).blank?
-
-    @records = if records.is_a?(Class)
-                 ForceUpdateAppVersion.where(value.query)
-               else
-                 records.or(ForceUpdateAppVersion.where('version like ?', "%#{params.dig(:force_update_app_versions, :version)}"))
-               end
-  end
-
-  def force_update_equal
-    return if params.dig(:force_update_app_versions, :force_update).blank?
-
-    @records = if records.is_a?(Class)
-                 ForceUpdateAppVersion.where(value.query)
-               else
-                 records.or(ForceUpdateAppVersion.where('force_update = ?', params.dig(:force_update_app_versions, :force_update)))
-               end
-  end
-
   def order
     return if records.blank?
-
     @records = records.order('force_update_app_versions.created_at desc')
   end
-
   def paginate
-    @records = ForceUpdateAppVersion.none if records.blank? || records.is_a?(Class)
+    @records = ForceUpdateAppVersion.none if records.blank?
     @records = records.page(params.dig(:pagination_page) || 1).per(params.dig(:pagination_limit) || 20)
   end
 end
