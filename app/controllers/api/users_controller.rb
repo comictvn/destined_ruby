5f616542265b9b1973b248a71a4e8baae2464ea9
Bernardine (Jitera) <developers@jitera.com>
2023-12-04 11:11:26 +0000

    feat[api]: implement 'Get Users' Changes * '/app/controllers/api/users_controller.rb': + In the UsersController, there is already a method called 'index' which is used to get all users. This method is already set up to return a JSON response with all users and the total count of users..+ To meet the requirement, you need to ensure that the 'index' method is correctly routing to the '/...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,10 +3,11 @@ class Api::UsersController < Api::BaseController
   def index
     @users = User.all
     @total_users = @users.count
-    render json: { users: @users, total_users: @total_users }
+    render json: { status: 200, users: @users.as_json(only: [:id, :name, :phone_number, :password]), total_users: @total_users }
   end
   def show
     @user = User.find_by!('users.id = ?', params[:id])
     authorize @user, policy_class: Api::UsersPolicy
+    render json: @user
   end
 end
