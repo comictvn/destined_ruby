3da2a34802fe2dda2322bce1118251f48ff91f27
Mattie (Jitera) <developers@jitera.com>
2023-12-04 12:51:58 +0000

    feat[api]: implement 'Create User Reset Password Request' Changes * '/app/controllers/api/users_reset_password_requests_controller.rb': + Create a new controller file named "users_reset_password_requests_controller.rb" under the "/app/controllers/api" directory..+ In this file, define a new class "UsersResetPasswordRequestsController" that inherits from "BaseController"..+ Define a "create" method that will handle the POST request t...

--- app/controllers/api/users_reset_password_requests_controller.rb
+++ app/controllers/api/users_reset_password_requests_controller.rb
@@ -2,15 +2,25 @@ class Api::UsersResetPasswordRequestsController < Api::BaseController
   def create
     user_id = params[:user_id]
     otp = params[:otp]
+    # Validate the otp parameter
+    if otp.blank?
+      render json: { error: 'OTP is required' }, status: :bad_request
+      return
+    end
     user = UserService::Index.call(user_id: user_id)
     if user.nil?
       render json: { error: 'User not found' }, status: :not_found
     else
-      reset_password_request = UserResetPasswordService.call(user_id: user_id, otp: otp)
-      if reset_password_request.persisted?
-        render json: { status: reset_password_request.status }, status: :ok
-      else
-        render json: { error: reset_password_request.errors.full_messages }, status: :unprocessable_entity
+      begin
+        reset_password_request = UserResetPasswordService.call(user_id: user_id, otp: otp)
+        if reset_password_request.persisted?
+          render json: { status: reset_password_request.status, reset_password_request: reset_password_request }, status: :ok
+        else
+          render json: { error: reset_password_request.errors.full_messages }, status: :unprocessable_entity
+        end
+      rescue => e
+        # Handle unexpected server error
+        render json: { error: 'An unexpected error occurred' }, status: :internal_server_error
       end
     end
   end
