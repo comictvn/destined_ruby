62eb1c64b52d0766092a62220b0b360bad99272c
Natalie (Jitera) <developers@jitera.com>
2023-12-04 12:46:20 +0000

    feat[business-logic]: implement 'Get users' Changes * '/app/controllers/api/users_controller.rb': + Create a new method named 'index' in the UsersController. This method will handle the request to get all users..+ Inside the 'index' method, call the UserService's 'index' method to retrieve all user records from the database..+ Render the retrieved user records in JSON format using the 'index.jso... * '/app/views/api/users/index.json.jbuilder': + Create a new 'index.json.jbuilder' view in the 'app/views/api/users' directory..+ In this view, iterate over each user record and display the desired attributes. Also, display the total number of user records.... * '/app/services/user_service/index.rb': + Create a new method named 'index' in the UserService. This method will retrieve all user records from the database..+ Use ActiveRecord's 'all' method on the User model to retrieve all user records.... * '/config/routes.rb': + Add a new route to handle the 'GET /users' request. This route should point to the 'index' method in the UsersController....

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,12 @@
 class Api::UsersController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[index show]
-
   def index
-    # inside service params are checked and whiteisted
-    @users = UserService::Index.new(params.permit!, current_resource_owner).execute
-    @total_pages = @users.total_pages
+    @users = User.all
+    @total_users = @users.count
+    render 'index.json.jbuilder'
   end
-
   def show
     @user = User.find_by!('users.id = ?', params[:id])
-
     authorize @user, policy_class: Api::UsersPolicy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   use_doorkeeper do
     controllers tokens: 'tokens'
-
     skip_controllers :authorizations, :applications, :authorized_applications
   end
-
   devise_for :users
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
@@ -13,47 +11,35 @@ Rails.application.routes.draw do
     resources :me, only: [:index] do
     end
   end
-
   namespace :api, defaults: { format: :json } do
     resources :force_update_app_versions, only: [:index] do
     end
-
     resources :users_verify_confirmation_token, only: [:create] do
     end
-
     resources :users_passwords, only: [:create] do
     end
-
     resources :users_registrations, only: [:create] do
     end
-
     resources :users_verify_reset_password_requests, only: [:create] do
     end
-
     resources :users_reset_password_requests, only: [:create] do
     end
-
     resources :messages, only: [:create] do
     end
-
     resources :chanels, only: %i[index show destroy] do
       resources :messages, only: %i[index destroy] do
       end
     end
-
     resources :verify_otp, only: [:create] do
     end
-
     resources :send_otp_codes, only: [:create] do
     end
-
     resources :users_phone_registrations, only: [:create] do
     end
-
     resources :users, only: %i[index show] do
     end
   end
-
   get '/health' => 'pages#health_check'
   get 'api-docs/v1/swagger.yaml' => 'swagger#yaml'
+  get '/users' => 'users#index'
 end
