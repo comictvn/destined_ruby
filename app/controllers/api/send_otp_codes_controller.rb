8ce52794c4d585b0b600c0f82bdf1d62a64e850c
Henrieta (Jitera) <developers@jitera.com>
2023-12-04 17:50:18 +0000

    feat[api]: implement 'Resend OTP Code' Changes * '/app/controllers/api/send_otp_codes_controller.rb': + Create a new method `resend` in the `SendOtpCodesController` class..+ This method will first find the user by the phone number provided in the request parameters..+ If the user is not found or is already verified, it will return a JSON response with an error message and a status of `:not_found`..+...

--- app/controllers/api/send_otp_codes_controller.rb
+++ app/controllers/api/send_otp_codes_controller.rb
@@ -1,27 +1,28 @@
 class Api::SendOtpCodesController < Api::BaseController
+  before_action :authenticate_user!
   def create
     @phone_number = ::Auths::PhoneNumber.new({ phone_number: params.dig(:phone_number) })
     unless @phone_number.valid?
-      @success = false
-      @message = @phone_number.errors.full_messages
-      return
+      return render json: { error: 'Invalid phone number.' }, status: :unprocessable_entity
     end
     service = ::Auths::PhoneVerification.new(@phone_number.formatted_phone_number)
     if service.send_otp
-      @success = true
-      @message = I18n.t('phone_login.otp.send_otp_success')
+      render json: { status: 200, message: 'OTP sent successfully.' }, status: :ok
     else
-      @success = false
-      @message = I18n.t('common.otp.exceed_amount_sent_otp')
+      render json: { error: I18n.t('common.otp.exceed_amount_sent_otp') }, status: :bad_request
     end
   end
   def resend
-    user = User.find_by(id: params[:user_id])
-    return render json: { error: 'User not found or already verified' }, status: :not_found unless user && !user.is_verified
-    service = ::Auths::PhoneVerification.new(user.phone_number)
+    user = User.find_by(phone: params[:phone])
+    if user.nil? || user.is_verified
+      return render json: { error: 'User not found or already verified' }, status: :not_found
+    end
+    service = ::Auths::PhoneVerification.new(user.phone)
     new_otp_code = service.generate_otp_code
-    user.otp_codes.update(otp_code: new_otp_code, created_at: Time.now)
-    SendOtpCodeJob.perform_later(user.phone_number, new_otp_code)
+    user.otp_codes.create(otp_code: new_otp_code, is_verified: false)
+    SendOtpCodeJob.perform_later(user.phone, new_otp_code)
     render json: { otp_code: new_otp_code, user_id: user.id, is_verified: user.is_verified }, status: :ok
+  rescue StandardError => e
+    render json: { error: e.message }, status: :bad_request
   end
 end
