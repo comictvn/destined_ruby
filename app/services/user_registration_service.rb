a8855f49058e0c5dfba1dd668ddc96e0ad786815
Mavis (Jitera) <developers@jitera.com>
2023-12-04 17:42:27 +0000

    feat[api]: implement 'User Registration' Changes * '/app/services/user_registration_service.rb': + Add a new method `register_user` in the `UserRegistrationService` class. This method should take three parameters: `username`, `password`, and `email`..+ Validate the `username` to ensure it is not more than 50 characters. If it is, raise an error with the message "You cannot input more 50 charact...

--- app/services/user_registration_service.rb
+++ app/services/user_registration_service.rb
@@ -1,18 +1,19 @@
 require 'phonelib'
 require 'bcrypt'
 class UserRegistrationService
-  def register_user(email, password, password_confirmation)
+  def register_user(username, email, password)
+    raise 'The username is required.' if username.blank?
+    raise 'You cannot input more 50 characters.' if username.length > 50
     raise 'The email is required.' if email.blank?
     raise 'Invalid email format.' unless email =~ URI::MailTo::EMAIL_REGEXP
     raise 'Password must be at least 8 characters.' if password.length < 8
-    raise 'Password and password confirmation do not match.' if password != password_confirmation
     if User.exists?(email: email)
-      return { status: 'failed', message: 'Email already registered' }
+      return { status: 400, message: 'Email already registered' }
     else
       encrypted_password = BCrypt::Password.create(password)
-      user = User.create(email: email, encrypted_password: encrypted_password)
+      user = User.create(username: username, email: email, encrypted_password: encrypted_password)
       send_confirmation_email(user)
-      return { status: 'success', message: 'Registration successful', user: user }
+      return { status: 200, message: 'User was successfully registered.' }
     end
   end
   def send_confirmation_email(user)
