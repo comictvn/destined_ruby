26c7443ba02acd3393985b98e606b85c47bbcce8
Melosa (Jitera) <developers@jitera.com>
2023-12-11 14:58:16 +0000

    feat[business-logic]: implement 'User profile completion' Changes * '/app/services/user_service/update_profile.rb': + Update the `update_profile` method to include a step for storing `preference_data` in the `user_preferences` table..+ Ensure that the `preference_data` is validated similarly to how `age_preference`, `gender_preference`, `location_preference`, and `inter...

--- app/services/user_service/update_profile.rb
+++ app/services/user_service/update_profile.rb
@@ -3,14 +3,15 @@
 class UserService::UpdateProfile
   include ActiveModel::Validations
 
-  validate :validate_input_data
+  validate :validate_input_data, :validate_preference_data
 
-  def initialize(user_id, age, gender, location, interests)
+  def initialize(user_id, age, gender, location, interests, preference_data = nil)
     @user_id = user_id
     @age = age
     @gender = gender
     @location = location
     @interests = interests
+    @preference_data = preference_data
   end
 
   def update_profile
@@ -21,22 +22,35 @@ class UserService::UpdateProfile
       user.update!(age: @age, gender: @gender, location: @location)
       user.user_interests.destroy_all
       @interests.each do |interest_id|
-        UserInterest.create!(user_id: @user_id, interest_id: interest_id)
+        interest = Interest.find_or_create_by!(id: interest_id)
+        UserInterest.create!(user_id: @user_id, interest_id: interest.id)
       end
+      update_user_preferences(user) if @preference_data
     end
 
-    { success: 'Profile updated successfully' }
+    { success: 'Profile and preferences updated successfully' }
   rescue ActiveRecord::RecordInvalid => e
     { error: e.message }
   end
 
   private
 
+  def update_user_preferences(user)
+    user_preference = user.user_preference || user.build_user_preference
+    user_preference.update!(preference_data: @preference_data)
+  end
+
   def validate_input_data
     errors.add(:age, 'must be a number') unless @age.is_a?(Integer)
     errors.add(:gender, 'must be a valid gender') unless ['male', 'female', 'other'].include?(@gender.downcase)
     errors.add(:location, 'must be a string') unless @location.is_a?(String)
     errors.add(:interests, 'must be an array of numbers') unless @interests.is_a?(Array) && @interests.all? { |i| i.is_a?(Integer) }
+  end
+
+  def validate_preference_data
+    return if @preference_data.nil?
+    errors.add(:preference_data, 'must be a hash') unless @preference_data.is_a?(Hash)
+    # Add more specific validations for preference_data structure and content here
   end
 end
 # rubocop:enable Style/ClassAndModuleChildren
