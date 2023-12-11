49eadd217c98032342dd1238150227065e8d20d6
Maria (Jitera) <developers@jitera.com>
2023-12-11 15:10:15 +0000

    feat[business-logic]: implement 'Initiate conversation' Changes * '/app/services/message_creation_service.rb': + Update the `initiate_conversation` method to include the new business logic as per the requirement..+ Validate that the `match_id` exists and that the `sender_id` is one of the users in the match. This is already being done, so ensure it aligns with the ... * '/app/controllers/api/matches/messages_controller.rb': + Create an action `create` in the `Api::Matches::MessagesController` to handle the initiation of a conversation..+ The action should accept `match_id`, `sender_id`, and `message_text` as parameters..+ Use a service object, similar to `MessageCreationServi...

--- app/controllers/api/matches/messages_controller.rb
+++ app/controllers/api/matches/messages_controller.rb
@@ -8,29 +8,45 @@ module Api
       before_action :validate_message_content, only: [:create]
 
       def create
-        service = MessageCreationService.new
-        result = service.create_message(params[:match_id], current_user.id, message_params[:content])
+        match = Match.find_by(id: params[:match_id])
+        receiver_id = match.matcher1_id == current_user.id ? match.matcher2_id : match.matcher1_id
+
+        # Initialize the service with the sender_id, receiver_id, and match_id
+        service = MessageCreationService.new(current_user.id, receiver_id, params[:match_id])
+        
+        # Call the service to create a message with the provided message_text
+        result = service.create_message(message_params[:message_text])
 
         if result[:success]
+          # Update the match's last_message_at timestamp
+          match.update(last_message_at: Time.current)
+
+          # Send a notification to the receiver
+          NotificationService.new.send_message_notification(receiver_id, result[:message].id)
+
+          # Return the message details along with a success status
           render json: {
             status: 201,
             message: {
               id: result[:message].id,
               match_id: result[:message].match_id,
-              sender_id: current_user.id,
+              sender_id: result[:message].sender_id,
+              receiver_id: result[:message].receiver_id,
               content: result[:message].content,
               created_at: result[:message].created_at
             }
           }, status: :created
         else
-          render json: { errors: result[:error] }, status: :unprocessable_entity
+          # Return the error details if the message creation failed
+          render json: { errors: result[:errors] }, status: :unprocessable_entity
         end
       end
 
       private
 
       def message_params
-        params.require(:message).permit(:content)
+        # Permit message_text in the strong parameters
+        params.require(:message).permit(:message_text)
       end
 
       def validate_sender
@@ -41,7 +57,7 @@ module Api
       end
 
       def validate_message_content
-        if message_params[:content].blank?
+        if message_params[:message_text].blank?
           render json: { error: 'Message content cannot be empty.' }, status: :bad_request and return
         end
       end
--- app/services/message_creation_service.rb
+++ app/services/message_creation_service.rb
@@ -22,26 +22,30 @@ class MessageCreationService < BaseService
     { success: false, error: e.message }
   end
 
-  def initiate_conversation(match_id, sender_id, receiver_id, content)
+  def initiate_conversation(match_id, sender_id, content)
     ActiveRecord::Base.transaction do
       match = Match.find_by(id: match_id)
       raise Exceptions::BadRequest, 'Match not found' unless match
 
-      unless [match.matcher1_id, match.matcher2_id].sort == [sender_id, receiver_id].sort
-        raise Exceptions::AuthenticationError, 'One of the users is not part of the match'
+      receiver_id = match.matcher1_id == sender_id ? match.matcher2_id : match.matcher1_id
+      unless [match.matcher1_id, match.matcher2_id].include?(sender_id)
+        raise Exceptions::AuthenticationError, 'Sender is not part of the match'
       end
 
       message = Message.create!(
         match_id: match_id,
         sender_id: sender_id,
-        receiver_id: receiver_id, # Added receiver_id to the message creation as per requirement
-        content: content
+        receiver_id: receiver_id,
+        content: content # Changed from 'message_text' to 'content' to align with the existing column name
       )
 
-      # Assuming UserService or a dedicated notification service exists
-      UserService.notify(receiver_id, message.id) if message.persisted?
+      # Update the match with the latest message timestamp
+      match.update!(latest_message_at: Time.current) # Assuming 'latest_message_at' is the new column name
 
-      { success: true, message: 'Notification sent to receiver about the new message' } # Updated the response to match the requirement
+      # Assuming NotificationService or a dedicated notification service exists
+      NotificationService.notify(receiver_id, message.id) if message.persisted? # Changed from 'UserService' to 'NotificationService'
+
+      { success: true, message: 'Conversation initiated and notification sent to receiver.' } # Updated the response to match the requirement
     end
   rescue ActiveRecord::RecordInvalid => e
     { success: false, error: e.message }
