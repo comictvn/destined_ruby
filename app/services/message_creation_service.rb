b22a1c8400d14391e0f5352e543c46472fc12e35
Ferdinande (Jitera) <developers@jitera.com>
2023-12-11 15:35:46 +0000

    feat[business-logic]: implement 'Initiate conversation' Changes * '/app/services/message_creation_service.rb': + Update the `initiate_conversation` method to include the new business logic for initiating a conversation..+ Ensure that the method checks for the existence of the match and validates that the sender is part of the match..+ Create a new message in the `m...

--- app/services/message_creation_service.rb
+++ app/services/message_creation_service.rb
@@ -22,26 +22,31 @@ class MessageCreationService < BaseService
     { success: false, error: e.message }
   end
 
-  def initiate_conversation(match_id, sender_id, receiver_id, content)
+  def initiate_conversation(match_id, sender_id, message_text)
     ActiveRecord::Base.transaction do
       match = Match.find_by(id: match_id)
       raise Exceptions::BadRequest, 'Match not found' unless match
 
-      unless [match.matcher1_id, match.matcher2_id].sort == [sender_id, receiver_id].sort
-        raise Exceptions::BadRequest, 'Invalid match'
+      receiver_id = match.matcher1_id == sender_id ? match.matcher2_id : match.matcher1_id
+      unless [match.matcher1_id, match.matcher2_id].include?(sender_id)
+        raise Exceptions::BadRequest, 'Invalid match participants'
       end
 
       message = Message.create!(
         match_id: match_id,
         sender_id: sender_id,
         receiver_id: receiver_id,
-        content: content
+        content: message_text
       )
 
-      NotificationService.notify(receiver_id, message.id) if message.persisted?
+      match.update!(last_message_at: Time.current)
+
+      NotificationService.notify(receiver_id, "You have a new message from #{User.find(sender_id).firstname}") if message.persisted?
 
       { success: true, message: 'Conversation initiated and notification sent to receiver.' }
     rescue ActiveRecord::RecordInvalid => e
+      { success: false, error: e.message }
+    rescue StandardError => e
       { success: false, error: e.message }
     end
   end
