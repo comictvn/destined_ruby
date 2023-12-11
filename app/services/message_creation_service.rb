99f08f90f76e89a1b49e6990b84751c7005db26b
Odelia (Jitera) <developers@jitera.com>
2023-12-11 14:58:46 +0000

    feat[business-logic]: implement 'Initiate conversation' Changes * '/app/services/message_creation_service.rb': + Implement a method `initiate_conversation` with inputs "match_id", "sender_id", "receiver_id", and "content"..+ First, validate the existence of a match between "sender_id" and "receiver_id" using the "match_id" by calling the `Match.find_by` method. Rai...

--- app/services/message_creation_service.rb
+++ app/services/message_creation_service.rb
@@ -21,4 +21,29 @@ class MessageCreationService < BaseService
   rescue ActiveRecord::RecordInvalid => e
     { success: false, error: e.message }
   end
+
+  def initiate_conversation(match_id, sender_id, receiver_id, content)
+    ActiveRecord::Base.transaction do
+      match = Match.find_by(id: match_id)
+      raise Exceptions::BadRequest, 'Match not found' unless match
+
+      unless [match.matcher1_id, match.matcher2_id].sort == [sender_id, receiver_id].sort
+        raise Exceptions::AuthenticationError, 'One of the users is not part of the match'
+      end
+
+      message = Message.create!(
+        match_id: match_id,
+        sender_id: sender_id,
+        receiver_id: receiver_id, # Added receiver_id to the message creation as per requirement
+        content: content
+      )
+
+      # Assuming UserService or a dedicated notification service exists
+      UserService.notify(receiver_id, message.id) if message.persisted?
+
+      { success: true, message: 'Notification sent to receiver about the new message' } # Updated the response to match the requirement
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    { success: false, error: e.message }
+  end
 end
