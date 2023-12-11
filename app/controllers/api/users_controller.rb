35cdb050882a7887291f43ff93b16fe99a84c1ea
Pansie (Jitera) <developers@jitera.com>
2023-12-11 14:55:41 +0000

    feat[api]: implement 'Complete User Profile' Changes * '/app/controllers/api/users_controller.rb': + First, ensure that the UsersController under the 'api' namespace exists. If not, create it by extending ApplicationController..+ Add a new method `update_profile` to handle the PUT request to '/api/users/{id}/profile'. This method will be responsible for...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,8 +1,9 @@
 class Api::UsersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index show matches]
+  before_action :doorkeeper_authorize!, only: %i[index show update_profile matches]
   before_action :set_user, only: [:matches]
 
   def index
+    # inside service params are checked and whitelisted
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
   end
@@ -10,6 +11,34 @@ class Api::UsersController < Api::BaseController
   def show
     @user = User.find_by!('users.id = ?', params[:id])
     authorize @user, policy_class: Api::UsersPolicy
+  end
+
+  # PUT /api/users/:id/profile
+  def update_profile
+    # Validate the input parameters
+    unless valid_update_profile_params?
+      render json: { error: "Invalid parameters" }, status: :bad_request
+      return
+    end
+
+    service = UserService::UpdateProfile.new(
+      user_id: params[:id],
+      age: params[:age],
+      gender: params[:gender],
+      location: params[:location],
+      interests: params[:interests],
+      answers: params[:answers]
+    )
+    
+    result = service.update_profile
+    
+    if result[:error].present?
+      render json: { error: result[:error] }, status: :unprocessable_entity
+    else
+      render json: { status: 200, message: "Profile updated successfully." }, status: :ok
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "User not found" }, status: :not_found
   end
 
   def matches
@@ -33,7 +62,33 @@ class Api::UsersController < Api::BaseController
 
   private
 
+  def doorkeeper_authorize!
+    # Implement user authentication logic here, possibly using OAuthTokensConcern
+  end
+
   def set_user
     @user = User.find_by(id: params[:id])
+  end
+
+  def valid_update_profile_params?
+    # Validate age
+    return false unless params[:age].is_a?(Integer) && params[:age] > 0
+    # Validate gender
+    return false unless User.genders.keys.include?(params[:gender])
+    # Validate location
+    return false unless params[:location].is_a?(String) && !params[:location].empty?
+    # Validate interests
+    return false unless params[:interests].is_a?(Array) && params[:interests].all? { |i| i.is_a?(Integer) }
+    # Validate answers
+    if params[:answers].is_a?(Array)
+      params[:answers].all? do |answer|
+        answer.is_a?(Hash) &&
+        answer.key?('question_id') && answer['question_id'].is_a?(Integer) &&
+        answer.key?('content') && answer['content'].is_a?(String)
+      end
+    else
+      return false
+    end
+    true
   end
 end
