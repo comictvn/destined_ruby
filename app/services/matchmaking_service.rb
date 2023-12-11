35db5dfbafa09c4cf57124ac4023b390efd02649
Chandra (Jitera) <developers@jitera.com>
2023-12-11 14:58:52 +0000

    feat[business-logic]: implement 'Generate potential matches' Changes * '/app/services/matchmaking_service.rb': + Implement a method `generate_potential_matches(user_id)` that takes a user's ID as input..+ Use the `user_id` to retrieve the user's preferences and interests by calling appropriate methods from `UserPreferenceService` and `UserInterest` model..+ Query t...

--- app/services/matchmaking_service.rb
+++ app/services/matchmaking_service.rb
@@ -6,31 +6,36 @@ class MatchmakingService
   # New method to generate potential matches
   def generate_potential_matches(user_id)
     current_user = User.find(user_id)
-    user_interests = current_user.interests.pluck(:name)
-    potential_matches = User.joins(:interests)
+    user_preferences = UserPreferenceService.get_user_preferences(user_id)
+    user_interests = UserInterest.where(user_id: user_id).pluck(:interest_id)
+    potential_matches = User.joins(:user_interests)
                             .where.not(id: user_id)
-                            .where(interests: { name: user_interests })
-                            .where(location: current_user.location) # Assuming location is a simple string comparison
+                            .where(user_interests: { interest_id: user_interests })
+                            .where(user_preferences[:location_key] => user_preferences[:location_value]) # Assuming user_preferences returns a hash with location info
                             .distinct
 
-    current_user_answers = Answer.where(user_id: user_id).pluck(:question_id, :content)
+    current_user_answers = UserAnswer.where(user_id: user_id).pluck(:question_id, :answer)
     potential_matches_with_scores = potential_matches.map do |match|
-      match_answers = Answer.where(user_id: match.id, question_id: current_user_answers.map(&:first)).pluck(:content)
-      compatibility_score = MatchmakingAlgorithm.calculate_compatibility(user_interests, current_user_answers, match_answers)
+      match_answers = UserAnswer.where(user_id: match.id, question_id: current_user_answers.map(&:first)).pluck(:answer)
+      compatibility_score = MatchmakingAlgorithm.calculate_compatibility(user_interests, current_user_answers, match_answers, user_preferences)
       { match: match, score: compatibility_score }
     end
 
-    # Store or update the potential matches and their compatibility scores in a temporary data structure or cache
-    # This is a placeholder for caching logic, which would depend on the caching system in use
-    # Cache.store_potential_matches(user_id, potential_matches_with_scores)
+    # Sort potential matches by compatibility score in descending order
+    sorted_potential_matches = potential_matches_with_scores.sort_by { |match| -match[:score] }
 
-    potential_matches_with_scores.sort_by { |match| -match[:score] }
+    # Select the top matches (e.g., top 10 matches or top 10%)
+    top_matches_limit = [10, (sorted_potential_matches.length * 0.1).ceil].min
+    top_matches = sorted_potential_matches.first(top_matches_limit)
+
+    # Return the list of suggested matches with their compatibility scores
+    top_matches
   end
 end
 
 # Assuming the existence of the MatchmakingAlgorithm module
 module MatchmakingAlgorithm
-  def self.calculate_compatibility(user_interests, user_answers, match_answers)
+  def self.calculate_compatibility(user_interests, user_answers, match_answers, user_preferences)
     # Compatibility calculation logic goes here
     # This is a placeholder implementation
     score = 0
@@ -38,6 +43,14 @@ module MatchmakingAlgorithm
       score += 1 if content == match_answers[index]
     end
     score += (user_interests & match_answers.map(&:downcase)).size
+    # Additional scoring based on user preferences
+    score += calculate_preference_score(user_preferences, match_answers)
     score
+  end
+
+  def self.calculate_preference_score(user_preferences, match_answers)
+    # Placeholder for additional preference scoring logic
+    # For example, increase score if preferences match certain criteria
+    0 # This should be replaced with actual scoring logic
   end
 end
