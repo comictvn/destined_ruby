88e63815ace590a6a19e770f0cf2c683a8d11383
Otha (Jitera) <developers@jitera.com>
2023-12-11 14:52:29 +0000

    feat[api]: implement 'Generate Potential Matches' Changes * '/app/controllers/api/users_controller.rb': + First, ensure that the `UsersController` class within the `/app/controllers/api/users_controller.rb` file is properly set up to handle API requests. If the file or class does not exist, create them accordingly..+ Within the `UsersController`, create a ne...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,15 +1,39 @@
 class Api::UsersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index show]
+  before_action :doorkeeper_authorize!, only: %i[index show matches]
+  before_action :set_user, only: [:matches]
 
   def index
-    # inside service params are checked and whiteisted
     @users = UserService::Index.new(params.permit!, current_resource_owner).execute
     @total_pages = @users.total_pages
   end
 
   def show
     @user = User.find_by!('users.id = ?', params[:id])
-
     authorize @user, policy_class: Api::UsersPolicy
+  end
+
+  def matches
+    return render json: { error: 'User not found.' }, status: :bad_request unless @user
+
+    potential_matches = MatchmakingService.new.generate_potential_matches(@user.id)
+    formatted_matches = potential_matches.map do |match|
+      {
+        id: match[:match].id,
+        age: match[:match].age,
+        gender: match[:match].gender,
+        location: match[:match].location,
+        compatibility_score: match[:score]
+      }
+    end
+
+    render json: { status: 200, matches: formatted_matches }, status: :ok
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
   end
 end
