35bc3cdc72755c8916a22c4b364a8432bf644d15
Vanessa (Jitera) <developers@jitera.com>
2023-12-11 15:34:11 +0000

    feat[business-logic]: implement 'Initiate conversation' Changes * '/app/controllers/api/matches/messages_controller.rb': + Add a new method `initiate_conversation` in the `MessagesController` to handle the initiation of a conversation..+ This method should first verify that a match exists between `sender_id` and `receiver_id` by checking the `match_id` in the `matches` table...

--- app/controllers/api/matches/messages_controller.rb
+++ app/controllers/api/matches/messages_controller.rb
@@ -10,46 +10,62 @@ module Api
       before_action :validate_user_part_of_match, only: [:create]
 
       def create
-        match = Match.find_by(id: params[:match_id])
-        receiver_id = match.matcher1_id == current_user.id ? match.matcher2_id : match.matcher1_id
+        initiate_conversation do |match, receiver_id|
+          # Call the service to create a message with the provided message_text
+          result = service.create_message(message_params[:message_text])
 
-        # Initialize the service with the sender_id, receiver_id, and match_id
-        service = MessageCreationService.new(current_user.id, receiver_id, params[:match_id])
-        
-        # Call the service to create a message with the provided message_text
-        result = service.create_message(message_params[:message_text])
+          if result[:success]
+            # Update the match's last_message_at timestamp
+            match.update(last_message_at: Time.current)
 
-        if result[:success]
-          # Update the match's last_message_at timestamp
-          match.update(last_message_at: Time.current)
+            # Send a notification to the receiver
+            NotificationService.new.send_message_notification(receiver_id, result[:message].id)
 
-          # Send a notification to the receiver
-          NotificationService.new.send_message_notification(receiver_id, result[:message].id)
-
-          # Return the message details along with a success status
-          render json: {
-            status: 201,
-            message: "Message sent successfully.",
-            conversation: {
-              id: match.id,
-              match_id: match.id,
-              messages: [
-                {
-                  sender_id: current_user.id,
-                  receiver_id: receiver_id,
-                  message_text: result[:message].content,
-                  created_at: result[:message].created_at
-                }
-              ]
-            }
-          }, status: :created
-        else
-          # Return the error details if the message creation failed
-          render json: { errors: result[:errors] }, status: :unprocessable_entity
+            # Return the message details along with a success status
+            render json: {
+              status: 201,
+              message: "Message sent successfully.",
+              conversation: {
+                id: match.id,
+                match_id: match.id,
+                messages: [
+                  {
+                    sender_id: current_user.id,
+                    receiver_id: receiver_id,
+                    message_text: result[:message].content,
+                    created_at: result[:message].created_at
+                  }
+                ]
+              }
+            }, status: :created
+          else
+            # Return the error details if the message creation failed
+            render json: { errors: result[:errors] }, status: :unprocessable_entity
+          end
         end
       end
 
       private
+
+      def initiate_conversation
+        match = Match.find_by(id: params[:match_id])
+        if match && (match.matcher1_id == current_user.id || match.matcher2_id == current_user.id)
+          receiver_id = match.matcher1_id == current_user.id ? match.matcher2_id : match.matcher1_id
+          ActiveRecord::Base.transaction do
+            message = Message.create!(
+              match_id: match.id,
+              sender_id: current_user.id,
+              receiver_id: receiver_id,
+              content: message_params[:message_text]
+            )
+            yield(match, receiver_id) if block_given?
+          end
+        else
+          render json: { error: 'Match not found or you are not part of the match.' }, status: :not_found
+        end
+      rescue ActiveRecord::RecordInvalid => e
+        render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+      end
 
       def message_params
         # Permit message_text in the strong parameters
@@ -76,8 +92,15 @@ module Api
       end
 
       def validate_user_part_of_match
-        unless User.exists?(id: params[:sender_id]) && User.exists?(id: params[:receiver_id])
-          render json: { error: 'Invalid sender or receiver.' }, status: :bad_request and return
+        match = Match.find_by(id: params[:match_id])
+        if match
+          sender_id = current_user.id
+          receiver_id = match.matcher1_id == sender_id ? match.matcher2_id : match.matcher1_id
+          unless User.exists?(id: sender_id) && User.exists?(id: receiver_id)
+            render json: { error: 'Invalid sender or receiver.' }, status: :bad_request and return
+          end
+        else
+          render json: { error: 'Match not found.' }, status: :not_found and return
         end
       end
     end
