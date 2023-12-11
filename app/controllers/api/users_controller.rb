7b863e1c1b968b66f74da0efdf4a49cd5e519339
Evvy (Jitera) <developers@jitera.com>
2023-12-11 16:12:31 +0000

    feat[api]: implement 'Complete User Profile' Changes * '/app/controllers/api/users_controller.rb': + To implement the "Complete User Profile" API, you will need to update the `Api::UsersController` with a new action called `update_profile`. This action will handle the PUT request to the endpoint `/api/users/profile/{user_id}`..+ Import the `UserService:...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,13 +1,14 @@
 class Api::UsersController < Api::BaseController
-  before_action :doorkeeper_authorize!, only: %i[index show update_profile matches update_preferences complete_profile swipes record_swipe get_potential_matches]
-  before_action :set_user, only: [:matches, :update_preferences, :complete_profile, :record_swipe, :get_potential_matches]
-  before_action :authenticate_user!, only: [:update_preferences, :update_profile, :record_swipe, :get_potential_matches] # authenticate_user! is now used for :record_swipe and :get_potential_matches as well
-  before_action :validate_preferences, only: [:update_preferences]
-  before_action :validate_swipe_action, only: [:record_swipe]
+  before_action :doorkeeper_authorize!, only: %i[index show update_profile matches update_preferences complete_profile swipes record_swipe]
+  before_action :set_user, only: [:matches, :update_preferences, :complete_profile, :record_swipe, :update_profile] # Combined set_user actions
+  before_action :authenticate_user!, only: [:update_preferences, :record_swipe, :update_profile] # Combined authenticate_user! actions
+  before_action :validate_preferences, only: [:update_preferences] # validate_preferences is used here
+  before_action :validate_swipe_action, only: [:record_swipe] # validate_swipe_action is used here
+  before_action :validate_complete_profile, only: [:update_profile] # Added before_action to validate complete profile data
 
   require_dependency 'matchmaking_service'
-  require_dependency 'user_service/update_profile'
-  require_dependency 'user_preference_service'
+  require_dependency 'user_service/update_profile' # Import the UserService::UpdateProfile class
+  require_dependency 'user_preference_service' # Import the UserPreferenceService class
 
   # ... existing methods ...
 
@@ -23,77 +24,101 @@ class Api::UsersController < Api::BaseController
 
   # ... other existing methods ...
 
+  # PUT /api/users/:id/profile
   def update_profile
-    # ... existing update_profile method ...
-  end
-
-  def update_preferences
-    # ... existing update_preferences method ...
-  end
-
-  def complete_profile
-    # ... existing complete_profile method ...
-  end
-
-  def matches
-    # ... existing matches method ...
-  end
-
-  def record_swipe
-    # ... existing record_swipe method ...
-  end
-
-  # GET /api/users/:user_id/matches
-  def get_potential_matches
     return render json: { error: 'User not found' }, status: :not_found unless @user
 
+    unless valid_update_profile_params?
+      return render json: { error: 'Invalid profile data' }, status: :bad_request
+    end
+
+    interests = params[:interests]
+    preferences = params[:preferences]
+
     begin
-      potential_matches = MatchmakingService.new.generate_potential_matches(@user.id)
-      render json: { status: 200, matches: potential_matches }, status: :ok
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: 'User not found' }, status: :not_found
+      ActiveRecord::Base.transaction do
+        # Update user's age, gender, location
+        @user.update!(age: params[:age], gender: params[:gender], location: params[:location])
+
+        # Handle interests
+        interests.each do |interest_name|
+          interest = Interest.find_or_create_by!(name: interest_name)
+          UserInterest.find_or_create_by!(user: @user, interest: interest)
+        end
+
+        # Update preferences using UserPreferenceService
+        UserPreferenceService.new(@user).update_preferences(preferences)
+
+        render json: { status: 200, message: 'Profile updated successfully', user: @user.as_json.merge(interests: interests, preferences: preferences) }, status: :ok
+      end
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: e.record.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    rescue Pundit::NotAuthorizedError
+      render json: { error: 'Not authorized to update profile' }, status: :forbidden
     rescue StandardError => e
       render json: { error: e.message }, status: :internal_server_error
     end
   end
+
+  # ... rest of the existing methods ...
 
   private
 
   # ... existing private methods ...
 
   def authenticate_user!
-    # ... existing authenticate_user! method ...
+    # Assuming there's a method to authenticate the user
+    render json: { error: 'Unauthorized' }, status: :unauthorized unless current_user && current_user.id == params[:user_id].to_i
   end
 
   def validate_preferences
-    # ... existing validate_preferences method ...
-  end
-
-  def set_user_by_id
-    # ... existing set_user_by_id method ...
-  end
-
-  def valid_update_profile_params?
-    # ... existing valid_update_profile_params? method ...
-  end
-
-  def valid_interests_params?(interests)
-    # ... existing valid_interests_params? method ...
-  end
-
-  def valid_preferences_params?(preferences)
-    # ... existing valid_preferences_params? method ...
+    preferences = params[:preference_data] || params[:preferences]
+    unless preferences.is_a?(Hash) && preferences.keys.all? { |k| k.is_a?(String) } || preferences.is_a?(String)
+      render json: { error: 'Invalid preferences format' }, status: :bad_request
+    end
+    # Add more validation rules as needed
   end
 
   def set_user
     @user = User.find_by(id: params[:user_id] || params[:id])
-    unless @user
-      render json: { error: 'User not found' }, status: :not_found and return
+  end
+
+  def validate_complete_profile
+    unless params[:age].is_a?(Integer) && params[:age] > 0
+      render json: { error: 'Invalid age.' }, status: :bad_request and return
+    end
+
+    unless User.genders.keys.include?(params[:gender])
+      render json: { error: 'Invalid gender type.' }, status: :bad_request and return
+    end
+
+    unless params[:location].is_a?(String)
+      render json: { error: 'Invalid location format.' }, status: :bad_request and return
+    end
+
+    unless valid_interests_params?(params[:interests])
+      render json: { error: 'Invalid interests.' }, status: :bad_request and return
+    end
+
+    unless valid_preferences_params?(params[:preferences])
+      render json: { error: 'Invalid preferences format.' }, status: :bad_request and return
     end
   end
 
+  def valid_update_profile_params?
+    params[:age].is_a?(Integer) && params[:gender].present? && params[:location].is_a?(String) && valid_interests_params?(params[:interests]) && valid_preferences_params?(params[:preferences])
+  end
+
+  def valid_interests_params?(interests)
+    interests.is_a?(Array) && interests.all? { |i| i.is_a?(String) }
+  end
+
+  def valid_preferences_params?(preferences)
+    preferences.is_a?(String)
+  end
+
   def swipe_params
-    # ... existing swipe_params method ...
+    params.permit(:target_user_id, :action)
   end
 
   def validate_swipe_action
