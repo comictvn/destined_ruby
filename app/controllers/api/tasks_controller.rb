6f76c1cd208f07fd143103cf59d7dd01cd6e3aaf
Danya (Jitera) <developers@jitera.com>
2023-12-18 09:05:57 +0000

    feat[business-logic]: implement 'Edit task' Changes * '/app/controllers/api/tasks_controller.rb': + Create an `update` action within the `TasksController` to handle the editing of tasks..+ Ensure that the `update` action receives the task "id" and the new attributes "title", "description", "due_date", and "status" as parameters..+ Use strong parameters...

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,6 +1,9 @@
 class Api::TasksController < Api::BaseController
   include ExceptionHandler
 
+  before_action :set_task, only: [:update]
+  before_action :authorize_task, only: [:update]
+
   def create
     # Ensure the user_id is taken from the current authenticated user
     @task = Task.new(task_params.merge(user_id: current_user.id, status: 'pending'))
@@ -16,9 +19,33 @@ class Api::TasksController < Api::BaseController
     end
   end
 
+  def update
+    if @task.update(update_task_params)
+      @task.touch # This line updates the 'updated_at' timestamp
+      render json: TaskSerializer.new(@task).serialized_json, status: :ok
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue Exceptions::AuthenticationError, Exceptions::BadRequest => e
+    render json: { error: e.message }, status: :forbidden
+  end
+
   private
+
+  def set_task
+    @task = Task.find_by(id: params[:id])
+    raise Exceptions::BadRequest.new("Task not found") if @task.nil?
+  end
+
+  def authorize_task
+    raise Exceptions::AuthenticationError.new("Not authorized to edit this task") unless TaskPolicy.new(current_user, @task).update?
+  end
 
   def task_params
     params.require(:task).permit(:title, :description, :due_date)
+  end
+
+  def update_task_params
+    params.require(:task).permit(:title, :description, :due_date, :status)
   end
 end
