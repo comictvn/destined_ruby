17db5ca2db2897d2df47966947fffdbff927046e
Pammy (Jitera) <developers@jitera.com>
2023-12-18 09:12:29 +0000

    feat[api]: implement 'Add New Task' Changes * '/app/controllers/api/tasks_controller.rb': + In the `Api::TasksController`, you will need to add a new action called `create` to handle the POST request to `/api/tasks`..+ Use the `task_params` private method to require and permit the necessary parameters (`title`, `description`, `due_date`). Ensur...

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,31 +1,28 @@
 class Api::TasksController < Api::BaseController
   include ExceptionHandler
 
-  before_action :set_task, only: [:update]
-  before_action :authorize_task, only: [:update]
-  before_action :authenticate_user!, only: [:index, :create, :update] # Ensure user is authenticated for index, create, and update actions
-
-  def index
-    # Retrieve the tasks belonging to the current user
-    tasks = Task.where(user_id: current_user.id)
-    render json: { status: 200, tasks: tasks.as_json(only: [:id, :title, :description, :due_date, :created_at, :status]) }, status: :ok
-  rescue ActiveRecord::RecordNotFound => e
-    render json: { error: e.message }, status: :not_found
-  end
+  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :set_task, only: [:update, :destroy]
+  before_action :authorize_task, only: [:update, :destroy]
 
   def create
-    # Ensure the user_id is taken from the current authenticated user
     @task = Task.new(task_params.merge(user_id: current_user.id, status: 'pending'))
     @task.created_at = Time.current
     @task.updated_at = Time.current
 
-    if @task.save
-      render json: TaskSerializer.new(@task).serialized_json, status: :created
+    if @task.valid?
+      if @task.save
+        render json: TaskSerializer.new(@task).serialized_json, status: :created
+      else
+        error_messages = @task.errors.messages
+        render json: { error_messages: error_messages, message: I18n.t('tasks.creation.failed') },
+               status: :unprocessable_entity
+      end
     else
-      error_messages = @task.errors.messages
-      render json: { error_messages: error_messages, message: I18n.t('tasks.creation.failed') },
-             status: :unprocessable_entity
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
     end
+  rescue ActiveRecord::RecordInvalid => e
+    render json: { error: e.message }, status: :bad_request
   end
 
   def update
@@ -39,14 +36,23 @@ class Api::TasksController < Api::BaseController
     render json: { error: e.message }, status: :forbidden
   end
 
-  private
-
-  def authenticate_user!
-    raise Exceptions::AuthenticationError.new("User must be authenticated") unless current_user
+  def destroy
+    if @task.destroy
+      render json: { status: 200, message: "Task successfully deleted." }, status: :ok
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessable_entity
+    end
+  rescue Exceptions::AuthenticationError, Exceptions::BadRequest => e
+    render json: { error: e.message }, status: :forbidden
   end
 
+  private
+
   def set_task
-    raise Exceptions::BadRequest.new("Invalid task ID format.") unless params[:id].match?(/^\d+$/)
+    unless params[:id].match?(/\A\d+\z/)
+      raise Exceptions::BadRequest.new("Invalid task ID format.")
+    end
+
     @task = Task.find_by(id: params[:id])
     raise Exceptions::BadRequest.new("Task not found") if @task.nil?
   end
@@ -56,17 +62,27 @@ class Api::TasksController < Api::BaseController
   end
 
   def task_params
-    params.require(:task).permit(:title, :description, :due_date)
+    params.require(:task).permit(:title, :description, :due_date).tap do |task_params|
+      raise Exceptions::BadRequest.new("Title is required.") if task_params[:title].blank?
+      raise Exceptions::BadRequest.new("Description is required.") if task_params[:description].blank?
+      begin
+        DateTime.parse(task_params[:due_date])
+      rescue ArgumentError
+        raise Exceptions::BadRequest.new("Invalid due date format.")
+      end
+    end
   end
 
   def update_task_params
     params.require(:task).permit(:title, :description, :due_date, :status).tap do |task_params|
       raise Exceptions::BadRequest.new("Title is required.") if task_params[:title].blank?
       raise Exceptions::BadRequest.new("Description is required.") if task_params[:description].blank?
-      begin
-        DateTime.parse(task_params[:due_date]) if task_params[:due_date].present?
-      rescue ArgumentError
-        raise Exceptions::BadRequest.new("Invalid due date format.")
+      if task_params[:due_date].present?
+        begin
+          DateTime.parse(task_params[:due_date])
+        rescue ArgumentError
+          raise Exceptions::BadRequest.new("Invalid due date format.")
+        end
       end
     end
   end
