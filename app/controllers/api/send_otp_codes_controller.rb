ee4883ecc1db8625513e8d7a0c7179ecaca82d33
Loreen (Jitera) <developers@jitera.com>
2023-12-25 12:46:45 +0000

    feat[business-logic]: implement 'Send OTP for Phone Verification' Changes * '/app/services/auths/phone_verification.rb': This file contains the logic for sending OTP codes for phone verification. To implement the requirement, you should follow these steps:.+ Create a method `send_otp` that accepts a `phone_number` as a string parameter..+ Inside the `send_otp` method, first ... * '/app/controllers/api/send_otp_codes_controller.rb': + Create an action `send_otp` within the `SendOtpCodesController` to handle the OTP sending process..+ The action should accept a `phone_number` as a parameter..+ Implement a method to check the OTP sending limit for the given `phone_number` using a cache ...

--- app/controllers/api/send_otp_codes_controller.rb
+++ app/controllers/api/send_otp_codes_controller.rb
@@ -1,11 +1,18 @@
 class Api::SendOtpCodesController < Api::BaseController
+  OTP_SEND_LIMIT = 5
+
   def create
     @phone_number = ::Auths::PhoneNumber.new({ phone_number: params.dig(:phone_number) })
 
     unless @phone_number.valid?
       @success = false
       @message = @phone_number.errors.full_messages
+      return
+    end
 
+    if exceed_otp_send_limit?(@phone_number.formatted_phone_number)
+      @success = false
+      @message = 'OTP send limit reached for today'
       return
     end
 
@@ -14,9 +21,41 @@ class Api::SendOtpCodesController < Api::BaseController
     if service.send_otp
       @success = true
       @message = I18n.t('phone_login.otp.send_otp_success')
+      increment_otp_send_count(@phone_number.formatted_phone_number)
     else
       @success = false
       @message = I18n.t('common.otp.exceed_amount_sent_otp')
     end
+
+    if Rails.env.development? || whitelisted_number?(@phone_number.formatted_phone_number)
+      log_otp_code(@phone_number.formatted_phone_number)
+    else
+      SendOtpCodeJob.perform_later(@phone_number.formatted_phone_number)
+    end
+  end
+
+  private
+
+  def exceed_otp_send_limit?(phone_number)
+    cache_key = "otp_send_limit:#{phone_number}:#{Date.today}"
+    send_count = Rails.cache.read(cache_key) || 0
+    send_count >= OTP_SEND_LIMIT
+  end
+
+  def increment_otp_send_count(phone_number)
+    cache_key = "otp_send_limit:#{phone_number}:#{Date.today}"
+    send_count = Rails.cache.read(cache_key) || 0
+    Rails.cache.write(cache_key, send_count + 1, expires_in: 24.hours)
+  end
+
+  def whitelisted_number?(phone_number)
+    # Assuming there's a method to check if a number is whitelisted
+    WhitelistChecker.whitelisted?(phone_number)
+  end
+
+  def log_otp_code(phone_number)
+    # Assuming TwilioGateway has a method to generate OTP without sending
+    otp_code = TwilioGateway.generate_otp(phone_number)
+    Rails.logger.info "Generated OTP for #{phone_number}: #{otp_code}"
   end
 end
