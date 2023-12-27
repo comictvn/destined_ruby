a6772a9111a6e7bac15537caf2c88c29e2d851c0
Jacquelin (Jitera) <developers@jitera.com>
2023-12-27 06:15:13 +0000

    feat[business-logic]: implement 'Generate potential matches' Changes * '/app/services/match_service.rb': + Implement a method `find_potential_matches(user_id)` within the `MatchService` class that takes a `user_id` as an input parameter..+ Within this method, retrieve the user's preferences and interests by joining the `users` table with the `preferences` tab...

--- app/services/match_service.rb
+++ app/services/match_service.rb
@@ -23,7 +23,6 @@ class MatchService
   private
 
   def retrieve_preferences_and_interests
-    # Assuming preferences have a method to retrieve interests
     @interests = @preferences.interests
   end
 
@@ -32,19 +31,16 @@ class MatchService
   end
 
   def find_compatible_users_within_distance
-    # Utilize the MatchingAlgorithm module to find users with similar preferences and interests within a specified distance
     @potential_matches = MatchingAlgorithm.find_compatible_users_within_distance(@user, @preferences, @user_answers, @interests)
   end
 
   def calculate_compatibility_scores
-    # Calculate compatibility scores for each potential match
     @potential_matches.each do |match|
       match[:score] = MatchingAlgorithm.calculate_compatibility(@user, match[:user], @preferences, match[:user_answers])
     end
   end
 
   def sort_and_select_top_matches
-    # Sort the potential matches by their compatibility scores and select the top matches
     @potential_matches.sort_by! { |match| -match[:score] }
     @potential_matches = @potential_matches.first(10) # Assuming we want the top 10 matches
   end
