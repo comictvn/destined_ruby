b1cec79fc60f9802938cbe15181e4d4ab1c9f62e
Astrid (Jitera) <developers@jitera.com>
2023-12-27 05:56:26 +0000

    feat[api]: implement 'Record Swipe Action' Changes * '/app/controllers/api/swipes_controller.rb': + In the `swipes_controller.rb` file, you will need to implement the `create` action to handle the POST request to `/api/users/{id}/swipes`..+ Import the `SwipeService` class from `/app/services/swipe_service.rb` to use the `record_swipe` method..+ Use the...

--- app/controllers/api/swipes_controller.rb
+++ app/controllers/api/swipes_controller.rb
@@ -6,12 +6,13 @@ class Api::SwipesController < Api::BaseController
 
   def create
     # Assuming SwipeService::Create exists and handles swipe logic
-    swipe = SwipeService::Create.new(@swiping_user, @swiped_user, @direction).execute
+    swipe_service = SwipeService.new
+    swipe_result = swipe_service.record_swipe(@swiping_user.id, @swiped_user.id, @direction)
 
-    if swipe.persisted?
+    if swipe_result[:swipe_recorded]
       render json: { status: 201, message: "Swipe recorded successfully." }, status: :created
     else
-      render json: { errors: swipe.errors.full_messages }, status: :unprocessable_entity
+      render json: { errors: swipe_result[:errors] || ["Failed to record swipe."] }, status: :unprocessable_entity
     end
   rescue => e
     render json: { error: e.message }, status: :internal_server_error
@@ -23,14 +24,14 @@ class Api::SwipesController < Api::BaseController
     @swiping_user = User.find_by(id: params[:id])
     @swiped_user = User.find_by(id: swipe_params[:swiped_id])
     unless @swiping_user && @swiped_user
-      render json: { error: "User not found." }, status: :not_found
+      render json: { error: "User not found." }, status: :not_found and return
     end
   end
 
   def validate_swipe_direction
     @direction = swipe_params[:direction]
     unless ['right', 'left'].include?(@direction)
-      render json: { error: "Invalid swipe direction." }, status: :bad_request
+      render json: { error: "Invalid swipe direction." }, status: :bad_request and return
     end
   end
 
