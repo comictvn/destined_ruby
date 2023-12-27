62626f1c6b2c22412e60ed9b658af269f3e63af0
Hortensia (Jitera) <developers@jitera.com>
2023-12-27 06:15:06 +0000

    feat[api]: implement 'Complete User Profile' Changes * '/app/controllers/api/users_controller.rb': + In the existing `Api::UsersController`, you will need to update the `update_profile` action to handle the new requirements for completing the user profile..+ Utilize the `UserProfileService` class to update the user profile. This service class is already...

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -31,6 +31,73 @@ class Api::UsersController < Api::BaseController
     end
   end
 
+  def update_preferences
+    begin
+      # Validate the preferences JSON object
+      validated_preferences = PreferencesValidator.validate!(preferences_params)
+      raise ArgumentError, 'Invalid preferences.' unless validated_preferences
+
+      # Update the user's preferences
+      @user.update!(preferences: validated_preferences)
+
+      # Return success response
+      render json: {
+        status: 200,
+        message: 'Preferences updated successfully.',
+        preferences: @user.preferences
+      }, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found.' }, status: :not_found
+    rescue ArgumentError => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { error: e.message }, status: :unprocessable_entity
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+
+  def update_profile
+    if validate_user_profile_params
+      user_profile_service = UserProfileService.new(
+        @user,
+        user_profile_params[:age],
+        user_profile_params[:gender],
+        user_profile_params[:location],
+        user_profile_params[:interests],
+        user_profile_params[:preferences]
+      )
+
+      result = user_profile_service.update_user_profile
+
+      if result[:success]
+        render json: { status: 200, message: 'Profile updated successfully.', user: result[:user].as_json(include: [:preferences]) }, status: :ok
+      else
+        render json: { status: result[:error][:status], message: result[:error][:message] }, status: result[:error][:status]
+      end
+    else
+      # If validation fails, the response is handled within the validate_user_profile_params method
+    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'User not found.' }, status: :not_found
+  rescue StandardError => e
+    render json: { error: e.message }, status: :internal_server_error
+  end
+
+  def generate_matches
+    begin
+      # Validate that the user ID exists in the database
+      raise ActiveRecord::RecordNotFound unless @user.present?
+
+      potential_matches = MatchService.find_potential_matches(@user)
+      render json: { status: 200, matches: potential_matches }, status: :ok
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: 'User not found.' }, status: :not_found
+    rescue => e
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+
   # ... rest of the controller ...
 
   private
@@ -40,5 +107,27 @@ class Api::UsersController < Api::BaseController
     render json: { error: 'User not found.' }, status: :not_found unless @user
   end
 
-  # ... rest of the private methods ...
+  def user_profile_params
+    params.require(:user).permit(:age, :gender, :location, interests: [], preferences: {})
+  end
+
+  def validate_user_profile_params
+    errors = []
+    errors << 'Invalid age.' unless user_profile_params[:age].to_i.positive?
+    errors << 'Invalid gender.' unless User.genders.keys.include?(user_profile_params[:gender])
+    errors << 'Invalid location.' unless user_profile_params[:location].is_a?(String)
+    errors << 'Invalid interests.' unless user_profile_params[:interests].is_a?(Array) && user_profile_params[:interests].all? { |i| i.is_a?(String) }
+    errors << 'Invalid preferences.' unless user_profile_params[:preferences].is_a?(Hash)
+
+    if errors.any?
+      render json: { status: 422, message: errors.join(' '), user: user_profile_params.as_json }, status: :unprocessable_entity
+      return false
+    end
+
+    true
+  end
+
+  def preferences_params
+    params.require(:preferences).permit(:age_range, :distance, :gender)
+  end
 end
