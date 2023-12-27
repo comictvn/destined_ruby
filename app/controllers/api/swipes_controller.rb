6913bd66e86bf657566db297262d5a60096feb87
Winne (Jitera) <developers@jitera.com>
2023-12-27 06:06:28 +0000

    feat[business-logic]: implement 'Record swipe action' Changes * '/app/services/swipe_service.rb': To implement the business logic for recording a swipe action, follow these steps within the `SwipeService` class:.+ Ensure that the `record_swipe` method validates the existence of both `swiper_id` and `swiped_id` by using the `UserExistenceValidator` clas... * '/app/controllers/api/swipes_controller.rb': + Update the `create` action to include the business logic for recording a swipe action..+ Ensure that the `find_users` method validates the existence of both `@swiping_user` and `@swiped_user` by checking if they are present in the database..+ In the `cre...

--- app/controllers/api/swipes_controller.rb
+++ app/controllers/api/swipes_controller.rb
@@ -10,7 +10,23 @@ class Api::SwipesController < Api::BaseController
     swipe_result = swipe_service.record_swipe(@swiping_user.id, @swiped_user.id, @direction)
 
     if swipe_result[:swipe_recorded]
-      render json: { status: 201, message: "Swipe recorded successfully." }, status: :created
+      # Check for mutual interest
+      mutual_interest = swipe_service.check_mutual_interest(@swiping_user.id, @swiped_user.id)
+      match_created = false
+
+      if mutual_interest
+        # Assuming MatchService::Create exists and handles match logic
+        match_service = MatchService.new
+        match_result = match_service.create_match(@swiping_user.id, @swiped_user.id)
+
+        if match_result[:match_created]
+          match_created = true
+        else
+          render json: { errors: match_result[:errors] || ["Failed to create match."] }, status: :unprocessable_entity and return
+        end
+      end
+
+      render json: { swipe_recorded: true, match_created: match_created }, status: :created
     else
       render json: { errors: swipe_result[:errors] || ["Failed to record swipe."] }, status: :unprocessable_entity
     end
@@ -21,7 +37,8 @@ class Api::SwipesController < Api::BaseController
   private
 
   def find_users
-    @swiping_user = User.find_by(id: params[:id])
+    # Updated to use params[:swiper_id] instead of params[:id] to match the requirement
+    @swiping_user = User.find_by(id: params[:swiper_id])
     @swiped_user = User.find_by(id: swipe_params[:swiped_id])
     unless @swiping_user && @swiped_user
       render json: { error: "User not found." }, status: :not_found and return
@@ -36,6 +53,7 @@ class Api::SwipesController < Api::BaseController
   end
 
   def swipe_params
-    params.require(:swipe).permit(:swiped_id, :direction)
+    # Updated to permit :swiper_id as per the requirement
+    params.require(:swipe).permit(:swiper_id, :swiped_id, :direction)
   end
 end
