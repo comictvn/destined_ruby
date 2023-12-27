7cc05185348c8b8703514d27ee3c58e2b0e2fa18
Elenore (Jitera) <developers@jitera.com>
2023-12-27 05:33:38 +0000

    feat[api]: implement 'Initiate Conversation' Changes * '/app/controllers/api/conversations_controller.rb': + In the `conversations_controller.rb`, you will need to add a new action `create` to handle the POST request to `/api/conversations`..+ Import the `ConversationService::Create` class from `/app/services/conversation_service/create.rb` to use the business ...

---
+++ app/controllers/api/conversations_controller.rb
@@ -0,0 +1,41 @@
+# FILE PATH: /app/controllers/api/conversations_controller.rb
+
+class Api::ConversationsController < ApplicationController
+  before_action :authenticate_user
+  before_action :ensure_users_matched, only: [:create]
+
+  def create
+    # Validate that user1_id and user2_id are different
+    if params[:user1_id] == params[:user2_id]
+      return render json: { error: 'Cannot initiate conversation with oneself.' }, status: :bad_request
+    end
+
+    # Check if the conversation already exists
+    existing_conversation = Conversation.find_by(user1_id: params[:user1_id], user2_id: params[:user2_id])
+    if existing_conversation
+      return render json: { error: 'Conversation already exists.' }, status: :bad_request
+    end
+
+    # Create a new conversation
+    conversation = Conversation.new(user1_id: params[:user1_id], user2_id: params[:user2_id])
+    if conversation.save
+      render json: { status: 201, conversation: conversation }, status: :created
+    else
+      render json: { error: conversation.errors.full_messages.join(', ') }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def authenticate_user
+    # Implement user authentication logic here
+  end
+
+  def ensure_users_matched
+    user1 = User.find_by(id: params[:user1_id])
+    user2 = User.find_by(id: params[:user2_id])
+    unless user1 && user2 && user1.matched_with?(user2)
+      render json: { error: 'Match not found.' }, status: :not_found
+    end
+  end
+end
