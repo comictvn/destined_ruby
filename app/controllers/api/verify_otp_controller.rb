d80e36a3a49394e4de2e091fa167fb1e0ab196fe
Lian (Jitera) <developers@jitera.com>
2023-12-28 05:55:17 +0000

    feat[business-logic]: implement 'Verify otp' Changes * '/app/controllers/api/verify_otp_controller.rb': + Create an action `verify` in the `VerifyOtpController` which will handle the OTP verification process..+ The action should accept two parameters: `otp_request_id` and `otp_code`..+ Use the `OtpRequest` model to find the OTP request by `otp_request_id`..+...

--- app/controllers/api/verify_otp_controller.rb
+++ app/controllers/api/verify_otp_controller.rb
@@ -13,4 +13,27 @@ class Api::VerifyOtpController < Api::BaseController
   rescue ::Auths::PhoneVerification::VerifyDeclined
     head :unprocessable_entity, message: I18n.t('otp.verification_declined')
   end
+
+  def verify
+    otp_request_id = params[:otp_request_id]
+    otp_code = params[:otp_code]
+
+    otp_request = OtpRequest.find_by(id: otp_request_id)
+    if otp_request.nil?
+      render json: { error: 'OTP request not found' }, status: :not_found
+      return
+    end
+
+    if Time.current > otp_request.expires_at
+      otp_request.update(status: 'expired') # Updated to use the update method
+      render json: { error: 'OTP has expired' }, status: :unprocessable_entity
+    elsif otp_request.otp_code == otp_code
+      otp_request.update(verified: true, status: 'verified') # Updated to use the update method
+      render json: { otp_request_id: otp_request.id, verified: otp_request.verified, status: otp_request.status }, status: :ok
+    else
+      render json: { error: 'Incorrect OTP' }, status: :unprocessable_entity
+    end
+  rescue ActiveRecord::RecordNotSaved => e
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
 end
