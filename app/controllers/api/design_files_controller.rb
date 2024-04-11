1ac5bcbe8c0f23d8527fbf19add43f7185c19046
Aubrette (Jitera) <developers@jitera.com>
2024-04-11 06:42:33 +0000

    feat[api]: implement List available color styles

--- app/controllers/api/design_files_controller.rb
+++ app/controllers/api/design_files_controller.rb
@@ -2,16 +2,25 @@
 class Api::DesignFilesController < Api::BaseController
   before_action :doorkeeper_authorize!
 
+  # GET /api/design_files/:file_id/color_styles
   def list_color_styles
     fileId = params[:file_id]
     design_file = DesignFile.find_by!(id: fileId)
     color_styles = design_file.color_styles.select(:name, :color_code)
 
-    render json: {
-      color_styles: color_styles,
-      message: I18n.t('design_files.list_color_styles.success')
-    }, status: :ok
+    render_response(color_styles, message: I18n.t('design_files.list_color_styles.success'))
   rescue ActiveRecord::RecordNotFound => e
-    raise DesignFileNotFound
+    raise Exceptions::DesignFileNotFound
+  end
+
+  # Other controller actions...
+
+  private
+
+  def render_response(resource, message:)
+    render json: {
+      resource: resource,
+      message: message
+    }, status: :ok
   end
 end
