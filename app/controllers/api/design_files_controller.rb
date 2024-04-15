9e93a5cc44b522d50aabc514f0be5b2fd8f49f00
Lynett (Jitera) <developers@jitera.com>
2024-04-15 18:08:48 +0000

    feat[api]: implement Display color styles icon

--- app/controllers/api/design_files_controller.rb
+++ app/controllers/api/design_files_controller.rb
@@ -3,17 +3,17 @@
 module Api
   class DesignFilesController < BaseController
     before_action :authenticate_user!
-    rescue_from Exceptions::LayerIneligibleError, with: :base_render_layer_ineligible_error
+    rescue_from Exceptions::LayerIneligibleError, with: :render_layer_ineligible_error
 
     def display_color_styles_icon(fileId, layerId)
       begin
         design_file = DesignFile.find(fileId)
         layer = design_file.layers.find(layerId)
         
-        # Check if the layer is eligible for color style application
-        raise Exceptions::LayerIneligibleError unless layer.eligible_for_color_styles?
+        # Checks if the layer is eligible for color styles
+        check_layer_eligibility(layer)
         
-        render_response({ display_color_styles_icon: true })
+        render_response({ display_color_styles_icon: true }, message: I18n.t('controller.display_color_styles_icon'))
       rescue ActiveRecord::RecordNotFound => e
         render_error('not_found', message: I18n.t('common.errors.record_not_found'), status: :not_found)
       rescue => e
@@ -23,11 +23,11 @@ module Api
 
     private
 
-    def base_render_layer_ineligible_error(exception)
+    def render_layer_ineligible_error(exception)
       render_error('layer_ineligible', message: I18n.t('controller.layer_not_eligible', default: exception.message), status: :unprocessable_entity)
     end
 
-    # This method should be defined in the Layer model
+    # Checks if the layer is eligible for color styles
     def check_layer_eligibility(layer)
       raise Exceptions::LayerIneligibleError unless layer.eligible_for_color_styles?
     end
@@ -38,10 +38,6 @@ end
 # app/models/layer.rb
 class Layer < ApplicationRecord
   # ... other code ...
-
-  def check_layer_eligibility
-    eligible_for_color_styles?
-  end
 
   def eligible_for_color_styles?
     # Placeholder for actual eligibility logic
--- app/models/layer.rb
+++ app/models/layer.rb
@@ -8,7 +8,13 @@ class Layer < ApplicationRecord
   validates :design_file_id, presence: true
 
   # Determines if the layer is eligible for color style application
-  def eligible_for_color_styles?
-    true # Placeholder logic, to be replaced with actual eligibility checks
+  def eligible_for_color_styles?    
+    # Placeholder logic for future eligibility checks
+    # For now, it always returns true
+    eligible = true
+
+    raise Exceptions::LayerIneligibleError, I18n.t('common.layer_not_eligible') unless eligible
+
+    eligible
   end
 end
