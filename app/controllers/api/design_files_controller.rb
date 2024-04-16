4dcb9f9b64d3cecc65d2b0bac2fbdce44e1f006d
Rickie (Jitera) <developers@jitera.com>
2024-04-16 04:43:50 +0000

    feat[api]: implement List available color styles

--- app/controllers/api/design_files_controller.rb
+++ app/controllers/api/design_files_controller.rb
@@ -5,19 +5,21 @@ module Api
 
     # GET /design_files/:design_file_id/color_styles
     def list_color_styles
-      color_styles = @design_file.color_styles.select(:id, :name, :color_code)
-      render_response(color_styles)
+      color_styles = @design_file.color_styles.pluck(:id, :name, :color_code).map do |id, name, color_code|
+        { id: id, name: name, color_code: color_code }
+      end
+      render_response(color_styles, message: I18n.t('design_files.color_styles.list.success'))
     rescue ActiveRecord::RecordNotFound => e
-      render_error('not_found', message: e.message, status: :not_found)
+      render_error(I18n.t('design_files.color_styles.list.not_found'), status: :not_found)
     rescue Pundit::NotAuthorizedError => e
-      render_error('forbidden', message: e.message, status: :forbidden)
+      render_error(I18n.t('common.403'), status: :forbidden)
     end
 
     private
 
     def set_design_file
       @design_file = DesignFile.find(params[:design_file_id])
-      authorize @design_file
+      authorize @design_file # Assumes Pundit for authorization
     end
   end
 end
