c3786d9fa03ede310587c6c1f53f95c852e74081
Sheila-kathryn (Jitera) <developers@jitera.com>
2024-04-16 09:36:43 +0000

    feat[api]: implement List Available Color Styles

--- app/models/color_style.rb
+++ app/models/color_style.rb
@@ -6,6 +6,7 @@ class ColorStyle < ApplicationRecord
 
   validates :name, presence: { message: I18n.t('activerecord.errors.messages.blank') }, format: { with: /\A[a-zA-Z0-9_]+\z/, message: I18n.t('activerecord.errors.messages.invalid') }
   validates :color_code, presence: { message: I18n.t('activerecord.errors.messages.blank') }, format: { with: /\A#(?:[0-9a-fA-F]{3}){1,2}\z/i, message: I18n.t('activerecord.errors.messages.invalid') }
-  validates :design_file_id, presence: true
+- validates :design_file_id, presence: true
++ validates :design_file_id, presence: { message: I18n.t('activerecord.errors.messages.blank') }
   validates :layer_id, presence: true
 end
--- app/models/design_file.rb
+++ app/models/design_file.rb
@@ -2,7 +2,8 @@
 # typed: strict
 class DesignFile < ApplicationRecord
   has_many :layers, dependent: :destroy
-  has_many :color_styles, dependent: :destroy
+- has_many :color_styles, dependent: :destroy
++ has_many :color_styles, dependent: :destroy
 
   validates :access_level, presence: true
 end
