2b75dd1348da27b0809f7a987459a739a2de6bfc
Robyn (Jitera) <developers@jitera.com>
2024-04-16 05:22:12 +0000

    feat[api]: implement Create new color style

--- app/controllers/api/design_files_controller.rb
+++ app/controllers/api/design_files_controller.rb
@@ -3,7 +3,6 @@ module Api
   class DesignFilesController < BaseController
     before_action :set_design_file, only: [:list_color_styles]
     rescue_from Exceptions::DesignFileNotFoundError, with: :design_file_not_found
-    before_action :validate_access_level, only: [:list_color_styles]
 
     # GET /design_files/:design_file_id/color_styles
     def list_color_styles
@@ -13,21 +12,20 @@ module Api
 
     # POST /design_files/:design_file_id/color_styles
     def create_color_style
-      validate_color_style_params
-      design_file = DesignFile.find_by(id: params[:design_file_id])
-      raise Exceptions::DesignFileNotFoundError unless design_file && can_modify_design_file?(design_file)
-
-      color_style = design_file.color_styles.create!(color_style_params)
+      service = ColorStyleCreationService.new(
+        name: color_style_params[:name],
+        color_code: color_style_params[:color_code],
+        design_file_id: color_style_params[:design_file_id]
+      )
+      color_style = service.call
       render_response(color_style, :created)
+    rescue Exceptions::DesignFileNotFoundError
+      render_error('not_found', message: I18n.t('design_files.color_styles.not_found'), status: :not_found)
+    rescue Exceptions::BadRequest => e
+      render_error('bad_request', message: e.message, status: :unprocessable_entity)
     end
 
     private
-
-    def validate_color_style_params
-      params.require(:name)
-      params.require(:color_code)
-      params.require(:design_file_id)
-    end
 
     def color_style_params
       params.permit(:name, :color_code, :design_file_id)
@@ -42,17 +40,8 @@ module Api
       render_error('forbidden', message: e.message, status: :forbidden)
     end
 
-    def validate_access_level
-      render_error(I18n.t('controller.design_files.unauthorized_access'), status: :unauthorized) unless current_user.has_access_to?(@design_file.access_level)
-    end
-
     def design_file_not_found
       render_error('not_found', message: I18n.t('design_files.color_styles.not_found'), status: :not_found)
-    end
-
-    def can_modify_design_file?(design_file)
-      # Placeholder for actual access level check
-      # This should contain the logic to check if the user has the necessary permissions to modify the design file
     end
   end
 end
--- app/models/color_style.rb
+++ app/models/color_style.rb
@@ -4,11 +4,9 @@ class ColorStyle < ApplicationRecord
   belongs_to :design_file
   belongs_to :layer
 
-  validates :name, presence: { message: I18n.t('activerecord.errors.models.color_style.attributes.name.blank') }
-  validates :color_code, presence: { message: I18n.t('activerecord.errors.models.color_style.attributes.color_code.blank') },
-                         format: { with: /\A#(?:[0-9a-fA-F]{3}){1,2}\z/, message: I18n.t('activerecord.errors.models.color_style.attributes.color_code.invalid_format') }
-  validates :design_file_id, presence: { message: I18n.t('activerecord.errors.models.color_style.attributes.design_file_id.blank') }
-  validates :layer_id, presence: { message: I18n.t('activerecord.errors.models.color_style.attributes.layer_id.blank') }
+  validates :name, presence: true
+  validates :color_code, presence: true, format: { with: /\A#(?:[0-9a-fA-F]{3}){1,2}\z/ }
+  validates :design_file_id, presence: true
 
   # Removed the validation for layer_id as it is not mentioned in the requirements
 end
