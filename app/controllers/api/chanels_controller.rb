1621789e843fde96f6f4f5f40907dd45d658a7da
Darleen (Jitera) <developers@jitera.com>
2024-08-21 06:39:06 +0000

    fix: 1 issues from deployment report

--- app/controllers/api/chanels_controller.rb
+++ app/controllers/api/chanels_controller.rb
@@ -1,22 +1,22 @@
-class Api::ChanelsController < Api::BaseController
+class Api::ChannelsController < Api::BaseController
   before_action :doorkeeper_authorize!, only: %i[index show destroy]
 
   def index
     # inside service params are checked and whiteisted
-    @chanels = ChanelService::Index.new(params.permit!, current_resource_owner).execute
-    @total_pages = @chanels.total_pages
+    @channels = ChannelService::Index.new(params.permit!, current_resource_owner).execute
+    @total_pages = @channels.total_pages
   end
 
   def show
-    @chanel = Chanel.find_by!('chanels.id = ?', params[:id])
+    @channel = Channel.find_by!('channels.id = ?', params[:id])
   end
 
   def destroy
-    @chanel = Chanel.find_by('chanels.id = ?', params[:id])
+    @channel = Channel.find_by('channels.id = ?', params[:id])
 
-    raise ActiveRecord::RecordNotFound if @chanel.blank?
+    raise ActiveRecord::RecordNotFound if @channel.blank?
 
-    if @chanel.destroy
+    if @channel.destroy
       head :ok, message: I18n.t('common.200')
     else
       head :unprocessable_entity
