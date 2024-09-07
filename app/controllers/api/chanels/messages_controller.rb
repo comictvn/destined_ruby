4c117ab2fe982350cbf54fb6099c3a81739de035
Corabel (Jitera) <developers@jitera.com>
2024-09-07 06:50:12 +0000

    fix: 1 issues from deployment report

--- app/controllers/api/chanels/messages_controller.rb
+++ app/controllers/api/chanels/messages_controller.rb
@@ -1,26 +1,24 @@
-module Chanels
-  module Chanels
-    class MessagesController < ApplicationController
-      before_action :doorkeeper_authorize!, only: %i[index destroy]
+module Api::Chanels
+  class MessagesController < ApplicationController
+    before_action :doorkeeper_authorize!, only: %i[index destroy]
 
-      def index
-        # inside service params are checked and whiteisted
-        @messages = MessageService::Index.new(params.permit!, current_resource_owner).execute
-        @total_pages = @messages.total_pages
-      end
+    def index
+      # inside service params are checked and whiteisted
+      @messages = MessageService::Index.new(params.permit!, current_resource_owner).execute
+      @total_pages = @messages.total_pages
+    end
 
-      def destroy
-        @message = Message.find_by(id: params[:id])
+    def destroy
+      @message = Message.find_by(id: params[:id])
 
-        raise ActiveRecord::RecordNotFound if @message.blank?
+      raise ActiveRecord::RecordNotFound if @message.blank?
 
-        authorize @message, policy_class: Chanels::MessagesPolicy
+      authorize @message, policy_class: Chanels::MessagesPolicy
 
-        if @message.destroy
-          head :ok, message: I18n.t('common.200')
-        else
-          head :unprocessable_entity, message: I18n.t('common.422')
-        end
+      if @message.destroy
+        head :ok, message: I18n.t('common.200')
+      else
+        head :unprocessable_entity, message: I18n.t('common.422')
       end
     end
   end
